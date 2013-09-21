46d3c75fcc159501b0b4ec8f9cbfb8390806859b
Guy Sela <guy.sela@contextream.com>
2013-09-22 00:50:26 +0300

    added integration test TELSDN-220
    Change-Id: If97527b0fc1e88d70f86fea5f8e13f3855857a02
    Signed-off-by: Guy Sela <guy.sela@contextream.com>

---
+++ mappingservice/integrationtest/src/test/java/org/opendaylight/lispflowmapping/integrationtest/MappingServiceIntegrationTest.java
@@ -0,0 +1,356 @@
+package org.opendaylight.lispflowmapping.integrationtest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemPackages;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+import javax.inject.Inject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opendaylight.lispflowmapping.implementation.serializer.LispMessage;
+import org.opendaylight.lispflowmapping.implementation.serializer.MapNotifySerializer;
+import org.opendaylight.lispflowmapping.type.lisp.MapNotify;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.util.PathUtils;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(PaxExam.class)
+public class MappingServiceIntegrationTest {
+
+    // private IMapResolver mapResolver;
+    // private IMapServer mapServer;
+    protected static final Logger logger = LoggerFactory.getLogger(MappingServiceIntegrationTest.class);
+    private byte[] mapRequestPacket;
+    private byte[] mapRegisterPacketWithNotify;
+    private byte[] mapRegisterPacketWithoutNotify;
+    int lispPortNumber = LispMessage.PORT_NUM;
+    String lispBindAddress = "127.0.0.1";
+    String ourAddress = "127.0.0.2";
+    private DatagramSocket socket;
+
+    @After
+    public void after() {
+        if (socket != null) {
+            socket.close();
+        }
+    }
+
+    @Before
+    public void before() throws Exception {
+        socket = initSocket(socket);
+        // mapResolver = context.mock(IMapResolver.class);
+        // mapServer = context.mock(IMapServer.class);
+
+        // SRC: 127.0.0.1:58560 to 127.0.0.1:4342
+        // LISP(Type = 8 - Encapsulated)
+        // IP: 192.168.136.10 -> 1.2.3.4
+        // UDP: 56756
+        // LISP(Type = 1 Map-Request
+        // Record Count: 1
+        // ITR-RLOC count: 0
+        // Source EID AFI: 0
+        // Source EID not present
+        // Nonce: 0x3d8d2acd39c8d608
+        // ITR-RLOC AFI=1 Address=192.168.136.10
+        // Record 1: 1.2.3.4/32
+        mapRequestPacket = extractWSUdpByteArray(new String("0000   00 00 00 00 00 00 00 00 00 00 00 00 08 00 45 00 " //
+                + "0010   00 58 00 00 40 00 40 11 3c 93 7f 00 00 01 7f 00 "
+                + "0020   00 01 e4 c0 10 f6 00 44 fe 57 80 00 00 00 45 00 "
+                + "0030   00 38 d4 31 00 00 ff 11 56 f3 c0 a8 88 0a 01 02 "
+                + "0040   03 04 dd b4 10 f6 00 24 ef 3a 10 00 00 01 3d 8d "
+                + "0050   2a cd 39 c8 d6 08 00 00 00 01 c0 a8 88 0a 00 20 " //
+                + "0060   00 01 01 02 03 04"));
+
+        // IP: 192.168.136.10 -> 128.223.156.35
+        // UDP: 49289 -> 4342
+        // LISP(Type = 3 Map-Register, P=1, M=1
+        // Record Counter: 1
+        // Nonce: 7
+        // Key ID: 0x0000 NO AUTHENTICATION!!
+        // AuthDataLength: 20 Data:
+        // e8:f5:0b:c5:c5:f2:b0:21:27:a8:21:41:04:f3:46:5a:a5:68:89:ec
+        // EID prefix: 153.16.254.1/32 (EID=0x9910FE01), TTL: 10, Authoritative,
+        // No-Action
+        // Local RLOC: 192.168.136.10 (RLOC=0xC0A8880A), Reachable,
+        // Priority/Weight: 1/100, Multicast Priority/Weight:
+        // 255/0
+        //
+
+        mapRegisterPacketWithNotify = extractWSUdpByteArray(new String("0000   00 50 56 ee d1 4f 00 0c 29 7a ce 79 08 00 45 00 " //
+                + "0010   00 5c 00 00 40 00 40 11 d4 db c0 a8 88 0a 80 df "
+                + "0020   9c 23 d6 40 10 f6 00 48 59 a4 38 00 01 01 00 00 "
+                + "0030   00 00 00 00 00 07 00 00 00 14 0e a4 c6 d8 a4 06 "
+                + "0040   71 7c 33 a4 5c 4a 83 1c de 74 53 03 0c ad 00 00 "
+                + "0050   00 0a 01 20 10 00 00 00 00 01 99 10 fe 01 01 64 " //
+                + "0060   ff 00 00 05 00 01 c0 a8 88 0a"));
+
+        // IP: 192.168.136.10 -> 128.223.156.35
+        // UDP: 49289 -> 4342
+        // LISP(Type = 3 Map-Register, P=1, M=1
+        // Record Counter: 1
+        // Nonce: 7
+        // Key ID: 0x0000 NO AUTHENTICATION!!
+        // AuthDataLength: 20 Data:
+        // e8:f5:0b:c5:c5:f2:b0:21:27:a8:21:41:04:f3:46:5a:a5:68:89:ec
+        // EID prefix: 153.16.254.1/32 (EID=0x9910FE01), TTL: 10, Authoritative,
+        // No-Action
+        // Local RLOC: 192.168.136.10 (RLOC=0xC0A8880A), Reachable,
+        // Priority/Weight: 1/100, Multicast Priority/Weight:
+        // 255/0
+        //
+
+        mapRegisterPacketWithoutNotify = extractWSUdpByteArray(new String("0000   00 50 56 ee d1 4f 00 0c 29 7a ce 79 08 00 45 00 " //
+                + "0010   00 5c 00 00 40 00 40 11 d4 db c0 a8 88 0a 80 df "
+                + "0020   9c 23 d6 40 10 f6 00 48 59 a4 38 00 00 01 00 00 "
+                + "0030   00 00 00 00 00 07 00 00 00 14 0e a4 c6 d8 a4 06 "
+                + "0040   71 7c 33 a4 5c 4a 83 1c de 74 53 03 0c ad 00 00 "
+                + "0050   00 0a 01 20 10 00 00 00 00 01 99 10 fe 01 01 64 " //
+                + "0060   ff 00 00 05 00 01 c0 a8 88 0a"));
+    }
+
+    @Inject
+    private BundleContext bc;
+
+    // Configure the OSGi container
+    @Configuration
+    public Option[] config() {
+        return options(
+                //
+                systemProperty("logback.configurationFile").value("file:" + PathUtils.getBaseDir() + "/src/test/resources/logback.xml"),
+                // To start OSGi console for inspection remotely
+                systemProperty("osgi.console").value("2401"),
+                // Set the systemPackages (used by clustering)
+                systemPackages("sun.reflect", "sun.reflect.misc", "sun.misc", "javax.crypto", "javax.crypto.spec"),
+
+                // OSGI infra
+                mavenBundle("org.jboss.spec.javax.transaction", "jboss-transaction-api_1.1_spec").versionAsInProject(),
+                mavenBundle("org.apache.commons", "commons-lang3").versionAsInProject(),
+                mavenBundle("org.apache.felix", "org.apache.felix.dependencymanager").versionAsInProject(),
+
+                // List logger bundles
+                mavenBundle("org.slf4j", "slf4j-api").versionAsInProject(), mavenBundle("org.slf4j", "log4j-over-slf4j").versionAsInProject(),
+                mavenBundle("ch.qos.logback", "logback-core").versionAsInProject(), mavenBundle("ch.qos.logback", "logback-classic")
+                        .versionAsInProject(),
+
+                mavenBundle("commons-io", "commons-io").versionAsInProject(),
+
+                mavenBundle("commons-fileupload", "commons-fileupload").versionAsInProject(),
+
+                mavenBundle("equinoxSDK381", "javax.servlet").versionAsInProject(), mavenBundle("equinoxSDK381", "javax.servlet.jsp")
+                        .versionAsInProject(), mavenBundle("equinoxSDK381", "org.eclipse.equinox.ds").versionAsInProject(),
+
+                mavenBundle("equinoxSDK381", "org.eclipse.equinox.util").versionAsInProject(),
+                mavenBundle("equinoxSDK381", "org.eclipse.osgi.services").versionAsInProject(),
+                mavenBundle("equinoxSDK381", "org.apache.felix.gogo.command").versionAsInProject(),
+                mavenBundle("equinoxSDK381", "org.apache.felix.gogo.runtime").versionAsInProject(),
+                mavenBundle("equinoxSDK381", "org.apache.felix.gogo.shell").versionAsInProject(),
+                mavenBundle("equinoxSDK381", "org.eclipse.equinox.cm").versionAsInProject(),
+                mavenBundle("equinoxSDK381", "org.eclipse.equinox.console").versionAsInProject(),
+                mavenBundle("equinoxSDK381", "org.eclipse.equinox.launcher").versionAsInProject(),
+
+                mavenBundle("org.apache.felix", "org.apache.felix.dependencymanager").versionAsInProject(),
+                mavenBundle("org.apache.felix", "org.apache.felix.dependencymanager.shell").versionAsInProject(),
+
+                mavenBundle("com.google.code.gson", "gson").versionAsInProject(),
+                mavenBundle("org.jboss.spec.javax.transaction", "jboss-transaction-api_1.1_spec").versionAsInProject(),
+                mavenBundle("org.apache.felix", "org.apache.felix.fileinstall").versionAsInProject(),
+                mavenBundle("org.apache.commons", "commons-lang3").versionAsInProject(), mavenBundle("commons-codec", "commons-codec")
+                        .versionAsInProject(), mavenBundle("virgomirror", "org.eclipse.jdt.core.compiler.batch").versionAsInProject(),
+                mavenBundle("eclipselink", "javax.persistence").versionAsInProject(), mavenBundle("eclipselink", "javax.resource")
+                        .versionAsInProject(),
+
+                mavenBundle("orbit", "javax.activation").versionAsInProject(),
+                mavenBundle("orbit", "javax.annotation").versionAsInProject(),
+                mavenBundle("orbit", "javax.ejb").versionAsInProject(),
+                mavenBundle("orbit", "javax.el").versionAsInProject(),
+                mavenBundle("orbit", "javax.mail.glassfish").versionAsInProject(),
+                mavenBundle("orbit", "javax.xml.rpc").versionAsInProject(),
+                mavenBundle("orbit", "org.apache.catalina").versionAsInProject(),
+
+                mavenBundle("orbit", "org.apache.catalina.ha").versionAsInProject().noStart(),
+                mavenBundle("orbit", "org.apache.catalina.tribes").versionAsInProject().noStart(),
+                mavenBundle("orbit", "org.apache.coyote").versionAsInProject().noStart(),
+                mavenBundle("orbit", "org.apache.jasper").versionAsInProject().noStart(),
+
+                mavenBundle("orbit", "org.apache.el").versionAsInProject(),
+                mavenBundle("orbit", "org.apache.juli.extras").versionAsInProject(),
+                mavenBundle("orbit", "org.apache.tomcat.api").versionAsInProject(),
+                mavenBundle("orbit", "org.apache.tomcat.util").versionAsInProject().noStart(),
+                mavenBundle("orbit", "javax.servlet.jsp.jstl").versionAsInProject(),
+                mavenBundle("orbit", "javax.servlet.jsp.jstl.impl").versionAsInProject(),
+
+                // Basic bundles needed
+                mavenBundle("org.opendaylight.controller", "containermanager").versionAsInProject(),
+                mavenBundle("org.opendaylight.controller", "containermanager.it.implementation").versionAsInProject(),
+
+                // Specific bundles
+                mavenBundle("org.opendaylight.controller", "clustering.services").versionAsInProject(),
+                mavenBundle("org.opendaylight.controller", "clustering.stub").versionAsInProject(),
+                mavenBundle("org.opendaylight.controller", "clustering.services").versionAsInProject(),
+                mavenBundle("org.opendaylight.controller", "sal").versionAsInProject(),
+                mavenBundle("org.opendaylight.lispflowmapping", "mappingservice.api").versionAsInProject(),
+                mavenBundle("org.opendaylight.lispflowmapping", "mappingservice.implementation").versionAsInProject(),
+                mavenBundle("org.opendaylight.lispflowmapping", "mappingservice.southbound").versionAsInProject(), junitBundles());
+    }
+
+    @Test
+    public void mapRegisterWithMapNotify() throws SocketTimeoutException {
+        sendPacket(mapRegisterPacketWithNotify);
+        ByteBuffer readBuf = ByteBuffer.wrap(receivePacket().getData());
+        MapNotify reply = MapNotifySerializer.getInstance().deserialize(readBuf);
+        assertEquals(7, reply.getNonce());
+    }
+
+    @Test
+    public void mapRegisterWithoutMapNotify() {
+        sendPacket(mapRegisterPacketWithoutNotify);
+        try {
+            receivePacket(3000);
+            // If didn't timeout then fail:
+            fail();
+        } catch (SocketTimeoutException ste) {
+        }
+    }
+
+    private void sendPacket(byte[] bytesToSend) {
+        try {
+            DatagramPacket packet = new DatagramPacket(bytesToSend, bytesToSend.length);
+            initPacketAddress(packet);
+            logger.info("Sending MapRegister to LispPlugin on socket");
+            socket.send(packet);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            fail();
+        }
+    }
+
+    private DatagramPacket receivePacket() throws SocketTimeoutException {
+        return receivePacket(5000);
+    }
+
+    private DatagramPacket receivePacket(int timeout) throws SocketTimeoutException {
+        try {
+            byte[] buffer = new byte[4096];
+            DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+            logger.info("Waiting for packet from socket...");
+            socket.setSoTimeout(timeout);
+            socket.receive(receivePacket);
+            logger.info("Recieved packet from socket!");
+            return receivePacket;
+        } catch (SocketTimeoutException ste) {
+            throw ste;
+        } catch (Throwable t) {
+            t.printStackTrace();
+            fail();
+            return null;
+        }
+    }
+
+    private void initPacketAddress(DatagramPacket packet) throws UnknownHostException {
+        packet.setAddress(InetAddress.getByName(lispBindAddress));
+        packet.setPort(lispPortNumber);
+    }
+
+    private DatagramSocket initSocket(DatagramSocket socket) {
+        try {
+            socket = new DatagramSocket(new InetSocketAddress(ourAddress, LispMessage.PORT_NUM));
+        } catch (SocketException e) {
+            e.printStackTrace();
+            fail();
+        }
+        return socket;
+    }
+
+    private byte[] extractWSUdpByteArray(String wiresharkHex) {
+        final int HEADER_LEN = 42;
+        byte[] res = new byte[1000];
+        String[] split = wiresharkHex.split(" ");
+        int counter = 0;
+        for (String cur : split) {
+            cur = cur.trim();
+            if (cur.length() == 2) {
+                ++counter;
+                if (counter > HEADER_LEN) {
+                    res[counter - HEADER_LEN - 1] = (byte) Integer.parseInt(cur, 16);
+                }
+
+            }
+        }
+        return Arrays.copyOf(res, counter - HEADER_LEN);
+    }
+
+    private String stateToString(int state) {
+        switch (state) {
+        case Bundle.ACTIVE:
+            return "ACTIVE";
+        case Bundle.INSTALLED:
+            return "INSTALLED";
+        case Bundle.RESOLVED:
+            return "RESOLVED";
+        case Bundle.UNINSTALLED:
+            return "UNINSTALLED";
+        default:
+            return "Not CONVERTED";
+        }
+    }
+
+    @Before
+    public void areWeReady() throws InvalidSyntaxException {
+        assertNotNull(bc);
+        boolean debugit = false;
+        Bundle b[] = bc.getBundles();
+        for (Bundle element : b) {
+            int state = element.getState();
+            if (state != Bundle.ACTIVE && state != Bundle.RESOLVED) {
+                System.out.println("Bundle:" + element.getSymbolicName() + " state:" + stateToString(state));
+                /*
+                 * UNCOMMENT to see why bundles didn't resolve! try { String
+                 * host = element.getHeaders().get(Constants.FRAGMENT_HOST); if
+                 * (host != null) { logger.warn("Bundle " +
+                 * element.getSymbolicName() +
+                 * " is a fragment which is part of: " + host);
+                 * logger.warn("Required imports are: " +
+                 * element.getHeaders().get(Constants.IMPORT_PACKAGE)); } else {
+                 * element.start(); } } catch (BundleException e) {
+                 * logger.error("BundleException:", e); fail(); }
+                 */
+                debugit = true;
+            }
+        }
+        if (debugit) {
+            logger.warn(("Do some debugging because some bundle is unresolved"));
+        }
+
+        // Uncomment this code to Know which services were actually loaded to
+        // BundleContext
+        /*
+         * for (ServiceReference sr : bc.getAllServiceReferences(null, null)) {
+         * logger.info(sr.getBundle().getSymbolicName());
+         * logger.info(sr.toString()); }
+         */
+    }
+}
