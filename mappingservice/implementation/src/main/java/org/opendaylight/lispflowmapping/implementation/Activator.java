bcec62c45bf1a3a77fb22cc6a8e1e7e7826e81dd
Guy Sela <guy.sela@contextream.com>
2013-10-21 02:31:56 +0200

    Converting the LispSB to MD-SAL plugin. The phase before writing everything in YANG. TELSDN-125
    Change-Id: I1f94e6482664a12c3179904bfeebd903e383ade2
    Signed-off-by: Guy Sela <guy.sela@contextream.com>

--- mappingservice/implementation/src/main/java/org/opendaylight/lispflowmapping/implementation/Activator.java
+++ mappingservice/implementation/src/main/java/org/opendaylight/lispflowmapping/implementation/Activator.java
@@ -13,10 +13,14 @@ import java.util.Hashtable;
 
 import org.apache.felix.dm.Component;
 import org.opendaylight.controller.clustering.services.IClusterContainerServices;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+import org.opendaylight.controller.sal.binding.api.BindingAwareConsumer;
 import org.opendaylight.controller.sal.core.ComponentActivatorAbstractBase;
 import org.opendaylight.lispflowmapping.implementation.dao.ClusterDAOService;
 import org.opendaylight.lispflowmapping.interfaces.dao.ILispDAO;
 import org.opendaylight.lispflowmapping.interfaces.lisp.IFlowMapping;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,6 +31,9 @@ import org.slf4j.LoggerFactory;
  */
 
 public class Activator extends ComponentActivatorAbstractBase {
+
+    private BundleContext context = null;
+    private LispMappingService instance = null;
 
     /*
      * Logger instance
@@ -51,6 +58,24 @@ public class Activator extends ComponentActivatorAbstractBase {
     public void destroy() {
     }
 
+    public void start(BundleContext context) {
+        super.start(context);
+        try {
+            ServiceReference<BindingAwareBroker> brokerRef = context.getServiceReference(BindingAwareBroker.class);
+            BindingAwareBroker broker = context.getService(brokerRef);
+            broker.registerConsumer((BindingAwareConsumer) instance, context);
+
+            ServiceReference<ILispDAO> lispRef = context.getServiceReference(ILispDAO.class);
+            ILispDAO service = context.getService(lispRef);
+            instance.setLispDao(service);
+
+            instance.init();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+    }
+
     /**
      * Function that is used to communicate to dependency manager the list of
      * known implementations for services inside a container
@@ -62,7 +87,7 @@ public class Activator extends ComponentActivatorAbstractBase {
      */
     @Override
     public Object[] getImplementations() {
-        Object[] res = { LispMappingService.class, ClusterDAOService.class };
+        Object[] res = { new LispMappingService(), ClusterDAOService.class };
         return res;
     }
 
@@ -83,20 +108,22 @@ public class Activator extends ComponentActivatorAbstractBase {
      */
     @Override
     public void configureInstance(Component c, Object imp, String containerName) {
-        if (imp.equals(LispMappingService.class)) {
+        if (imp instanceof LispMappingService) {
             // export the service
             Dictionary<String, String> props = new Hashtable<String, String>();
             props.put("name", "mappingservice");
             c.setInterface(new String[] { IFlowMapping.class.getName() }, props);
-            c.add(createContainerServiceDependency(containerName).setService(ILispDAO.class).setCallbacks("setLispDao", "unsetLispDao").setRequired(
-                    true));
+            c.add(createContainerServiceDependency(containerName).setService(ILispDAO.class).setCallbacks("setLispDao", "unsetLispDao")
+                    .setRequired(true));
+            c.add(createContainerServiceDependency(containerName).setService(BindingAwareBroker.class).setRequired(true));
+            this.instance = (LispMappingService) imp;
         } else if (imp.equals(ClusterDAOService.class)) {
             // export the service
             Dictionary<String, String> props = new Hashtable<String, String>();
             props.put("name", "clusterosgiservice");
             c.setInterface(new String[] { ILispDAO.class.getName() }, props);
-            c.add(createContainerServiceDependency(containerName).setService(IClusterContainerServices.class).setCallbacks(
-                    "setClusterContainerService", "unsetClusterContainerService").setRequired(true));
+            c.add(createContainerServiceDependency(containerName).setService(IClusterContainerServices.class)
+                    .setCallbacks("setClusterContainerService", "unsetClusterContainerService").setRequired(true));
         }
     }
 
