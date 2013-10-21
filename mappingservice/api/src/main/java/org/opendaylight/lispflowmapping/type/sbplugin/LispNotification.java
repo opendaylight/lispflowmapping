5171e31d06b8a42818086ccfe79af4f425690361
Guy Sela <guy.sela@contextream.com>
2013-10-21 02:31:56 +0200

    Converting the LispSB to MD-SAL plugin. The phase before writing everything in YANG. TELSDN-125
    Change-Id: I1f94e6482664a12c3179904bfeebd903e383ade2
    Signed-off-by: Guy Sela <guy.sela@contextream.com>

---
+++ mappingservice/api/src/main/java/org/opendaylight/lispflowmapping/type/sbplugin/LispNotification.java
@@ -0,0 +1,7 @@
+package org.opendaylight.lispflowmapping.type.sbplugin;
+
+import org.opendaylight.yangtools.yang.binding.Notification;
+
+public interface LispNotification extends Notification {
+
+}
