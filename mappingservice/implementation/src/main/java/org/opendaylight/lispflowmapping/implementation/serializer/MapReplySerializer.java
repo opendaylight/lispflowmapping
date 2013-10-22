df2b77fb962a7e675e0c215f48bc084b34b8809c
David Goldberg <david.goldberg@contextream.com>
2013-10-22 03:30:55 +0200

    support map reply deserialization TELSDN-379: #close Signed-off-by: David Goldberg <david.goldberg@contextream.com>
    Change-Id: Ife6a58d99e2efde1d2b1e6f9dacc9af794381055
    Signed-off-by: David Goldberg <david.goldberg@contextream.com>

--- mappingservice/implementation/src/main/java/org/opendaylight/lispflowmapping/implementation/serializer/MapReplySerializer.java
+++ mappingservice/implementation/src/main/java/org/opendaylight/lispflowmapping/implementation/serializer/MapReplySerializer.java
@@ -2,26 +2,26 @@ package org.opendaylight.lispflowmapping.implementation.serializer;
 
 import java.nio.ByteBuffer;
 
+import org.opendaylight.lispflowmapping.implementation.util.ByteUtil;
 import org.opendaylight.lispflowmapping.type.lisp.EidToLocatorRecord;
 import org.opendaylight.lispflowmapping.type.lisp.MapReply;
-
 
 /**
  * This class deals with serializing map reply from the java object to udp.
  */
 public class MapReplySerializer {
-	
-	private static final MapReplySerializer INSTANCE = new MapReplySerializer();
 
-	// Private constructor prevents instantiation from other classes
-	private MapReplySerializer() {
-	}
+    private static final MapReplySerializer INSTANCE = new MapReplySerializer();
 
-	public static MapReplySerializer getInstance() {
-		return INSTANCE;
-	}
-	
-	public ByteBuffer serialize(MapReply mapReply) {
+    // Private constructor prevents instantiation from other classes
+    private MapReplySerializer() {
+    }
+
+    public static MapReplySerializer getInstance() {
+        return INSTANCE;
+    }
+
+    public ByteBuffer serialize(MapReply mapReply) {
         int size = Length.HEADER_SIZE;
         for (EidToLocatorRecord eidToLocatorRecord : mapReply.getEidToLocatorRecords()) {
             size += EidToLocatorRecordSerializer.getInstance().getSerializationSize(eidToLocatorRecord);
@@ -37,12 +37,28 @@ public class MapReplySerializer {
         replyBuffer.put((byte) mapReply.getEidToLocatorRecords().size());
         replyBuffer.putLong(mapReply.getNonce());
         for (EidToLocatorRecord eidToLocatorRecord : mapReply.getEidToLocatorRecords()) {
-        	EidToLocatorRecordSerializer.getInstance().serialize(replyBuffer, eidToLocatorRecord);
+            EidToLocatorRecordSerializer.getInstance().serialize(replyBuffer, eidToLocatorRecord);
         }
         return replyBuffer;
     }
-	
-	private interface Length {
+
+    public MapReply deserialize(ByteBuffer replyBuffer) {
+        MapReply mapReply = new MapReply();
+        byte typeAndFlags = replyBuffer.get();
+        mapReply.setProbe(ByteUtil.extractBit(typeAndFlags, Flags.PROBE));
+        mapReply.setEchoNonceEnabled(ByteUtil.extractBit(typeAndFlags, Flags.ECHO_NONCE_ENABLED));
+        mapReply.setSecurityEnabled(ByteUtil.extractBit(typeAndFlags, Flags.SECURITY_ENABLED));
+        replyBuffer.getShort();
+        int recordCount = replyBuffer.get();
+        mapReply.setNonce(replyBuffer.getLong());
+        for (int i = 0; i < recordCount; i++) {
+            mapReply.addEidToLocator(EidToLocatorRecordSerializer.getInstance().deserialize(replyBuffer));
+        }
+
+        return mapReply;
+    }
+
+    private interface Length {
         int RES = 2;
         int HEADER_SIZE = 12;
     }
@@ -50,6 +66,7 @@ public class MapReplySerializer {
     private interface Flags {
         int PROBE = 0x08;
         int ECHO_NONCE_ENABLED = 0x04;
+        int SECURITY_ENABLED = 0x02;
     }
-	
+
 }
