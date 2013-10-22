4c2cd4d672679a25a05037fdb238b59769ee11bd
Guy Sela <guy.sela@contextream.com>
2013-10-22 22:32:41 +0200

    Make Notifications extend a different version of yang, to be like the Toaster TELSDN-504
    Change-Id: Ibc246a608015fd3e48800e1b58e09327116709f6
    Signed-off-by: Guy Sela <guy.sela@contextream.com>

--- mappingservice/api/src/main/java/org/opendaylight/lispflowmapping/type/sbplugin/LispNotification.java
+++ mappingservice/api/src/main/java/org/opendaylight/lispflowmapping/type/sbplugin/LispNotification.java
@@ -1,7 +1,8 @@
 package org.opendaylight.lispflowmapping.type.sbplugin;
 
+import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.Notification;
 
-public interface LispNotification extends Notification {
+public interface LispNotification extends Notification, DataObject {
 
 }
