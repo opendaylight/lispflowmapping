e632f1ba79d63f362507ef8fadfd4de9968638e3
Lorand Jakab <lojakab@cisco.com>
2014-04-30 14:21:11 +0300

    Centralize config.ini values into a class
    Change-Id: Ibcfe16a9edc4c2815ef74c9e856fd0c806ba6e30
    Signed-off-by: Lorand Jakab <lojakab@cisco.com>

---
+++ mappingservice/implementation/src/main/java/org/opendaylight/lispflowmapping/implementation/config/ConfigIni.java
@@ -0,0 +1,41 @@
+package org.opendaylight.lispflowmapping.implementation.config;
+
+public class ConfigIni {
+    private boolean mappingOverwrite;
+    private boolean smr;
+
+    public ConfigIni() {
+        initMappingOverwrite();
+        initSmr();
+    }
+
+    private void initMappingOverwrite() {
+        String str = System.getProperty("lisp.mappingOverwrite");
+        if (str != null) {
+            if (str.trim().equalsIgnoreCase("false")) {
+                this.mappingOverwrite = false;
+            } else {
+                this.mappingOverwrite = true;
+            }
+        }
+    }
+
+    private void initSmr() {
+        String str = System.getProperty("lisp.smr");
+        if (str != null) {
+            if (str.trim().equalsIgnoreCase("true")) {
+                this.smr = true;
+            } else {
+                this.smr = false;
+            }
+        }
+    }
+
+    public boolean mappingOverwriteIsSet() {
+        return mappingOverwrite;
+    }
+
+    public boolean smrIsSet() {
+        return smr;
+    }
+}
