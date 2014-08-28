a30ae1763d914a64047288873cea30c5a3c5b8c6
Vina Ermagan <vermagan@cisco.com>
2014-08-27 21:49:45 -0700

    Expand implementation of Neutron Service APIs in Lisp .
    Change-Id: I28e3a20439796fa436ea2cac62c7ff664d4a6ef3
    Signed-off-by: Vina Ermagan <vermagan@cisco.com>

--- mappingservice/neutron/src/main/java/org/opendaylight/lispflowmapping/neutron/ILispNeutronService.java
+++ mappingservice/neutron/src/main/java/org/opendaylight/lispflowmapping/neutron/ILispNeutronService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -11,7 +11,7 @@ package org.opendaylight.lispflowmapping.neutron;
 import org.opendaylight.lispflowmapping.interfaces.lisp.IFlowMapping;
 
 public interface ILispNeutronService {
-	
+
 	public IFlowMapping getMappingService();
 
 }
---
+++ mappingservice/neutron/src/main/java/org/opendaylight/lispflowmapping/neutron/LispNeutronNetworkHandler.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.lispflowmapping.neutron;
+
+import java.net.HttpURLConnection;
+
+import org.opendaylight.controller.networkconfig.neutron.INeutronNetworkAware;
+import org.opendaylight.controller.networkconfig.neutron.NeutronNetwork;
+
+/**
+ * LISP Service Implementation of NeutronNetworkAware API
+ *
+ */
+public class LispNeutronNetworkHandler extends LispNeutronService implements INeutronNetworkAware {
+
+
+	@Override
+	public int canCreateNetwork(NeutronNetwork network) {
+        logger.info("Neutron canCreateNetwork : Network name: " + network.getNetworkName());
+        logger.debug("Lisp Neutron Network: " + network.toString());
+
+        return HttpURLConnection.HTTP_OK;
+	}
+
+	@Override
+	public void neutronNetworkCreated(NeutronNetwork network) {
+        logger.info("Neutron Network Created : Network name: " + network.getNetworkName());
+        logger.debug("Lisp Neutron Network: " + network.toString());
+
+	}
+
+	@Override
+	public int canUpdateNetwork(NeutronNetwork delta, NeutronNetwork original) {
+        logger.info("Neutron canUpdateNetwork : Network name: " + original.getNetworkName());
+        logger.debug("Lisp Neutron Network: " + original.toString());
+
+        return HttpURLConnection.HTTP_OK;
+	}
+
+	@Override
+	public void neutronNetworkUpdated(NeutronNetwork network) {
+        logger.info("Neutron Network Updated : Network name: " + network.getNetworkName());
+        logger.debug("Lisp Neutron Network: " + network.toString());
+
+	}
+
+	@Override
+	public int canDeleteNetwork(NeutronNetwork network) {
+        logger.info("Neutron canDeleteNetwork : Network name: " + network.getNetworkName());
+        logger.debug("Lisp Neutron Network: " + network.toString());
+
+        return HttpURLConnection.HTTP_OK;
+	}
+
+	@Override
+	public void neutronNetworkDeleted(NeutronNetwork network) {
+        logger.info("Neutron Network Deleted : Network name: " + network.getNetworkName());
+        logger.debug("Lisp Neutron Network: " + network.toString());
+
+        return;
+	}
+
+}
