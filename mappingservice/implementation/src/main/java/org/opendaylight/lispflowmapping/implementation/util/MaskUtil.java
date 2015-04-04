6259acaf0e2f86706b64b9b9559ebab38531a02b
Florin Coras <fcoras@cisco.com>
2015-04-03 18:56:43 -0700

    Bug 2946: Make LcafSegmentAddr maskable
    This also enables longest prefix matches with segment addresses.
    
    Change-Id: Iaf2c626786e79482aa1983c5226c012529591542
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- mappingservice/implementation/src/main/java/org/opendaylight/lispflowmapping/implementation/util/MaskUtil.java
+++ mappingservice/implementation/src/main/java/org/opendaylight/lispflowmapping/implementation/util/MaskUtil.java
@@ -14,13 +14,20 @@ import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.control.plane.rev150314.LispAFIAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.control.plane.rev150314.lcafsegmentaddress.AddressBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.control.plane.rev150314.lcafsegmentaddress.Address;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.control.plane.rev150314.lispaddress.lispaddresscontainer.address.ipv4.Ipv4Address;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.control.plane.rev150314.lispaddress.lispaddresscontainer.address.ipv6.Ipv6Address;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.control.plane.rev150314.lispaddress.lispaddresscontainer.address.lcafsegment.LcafSegmentAddr;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.control.plane.rev150314.lispaddress.lispaddresscontainer.address.lcafsegment.LcafSegmentAddrBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MaskUtil {
-
+    private static final Logger LOG = LoggerFactory.getLogger(MaskUtil.class);
     public static boolean isMaskable(LispAFIAddress address) {
-        if (address instanceof Ipv4Address || address instanceof Ipv6Address) {
+        if (address instanceof Ipv4Address || address instanceof Ipv6Address
+                || address instanceof LcafSegmentAddr) {
             return true;
         }
         return false;
@@ -29,18 +36,34 @@ public class MaskUtil {
     public static LispAFIAddress normalize(LispAFIAddress address, int mask) {
         try {
             if (address instanceof Ipv4Address) {
-                return LispAFIConvertor.asIPAfiAddress(normalizeIP(Inet4Address.getByName(((Ipv4Address) address).getIpv4Address().getValue()),
-                        mask).getHostAddress());
+                return LispAFIConvertor.asIPAfiAddress(normalizeIP(Inet4Address.getByName(((Ipv4Address) address)
+                        .getIpv4Address().getValue()), mask).getHostAddress());
+            } else if (address instanceof Ipv6Address) {
+                return  LispAFIConvertor.asIPv6AfiAddress(normalizeIP(Inet6Address.getByName(((Ipv6Address) address)
+                        .getIpv6Address().getValue()), mask).getHostAddress());
+            } else if (address instanceof LcafSegmentAddr) {
+                LcafSegmentAddr segAddr = (LcafSegmentAddr) address;
+                Address normalizedAddr = new AddressBuilder().setPrimitiveAddress(LispAFIConvertor.toPrimitive(
+                        normalizeIpAddress(LispAFIConvertor.toAFIfromPrimitive(segAddr.getAddress()
+                        .getPrimitiveAddress()), mask))).build();
+                return new LcafSegmentAddrBuilder(segAddr).setAddress(normalizedAddr).build();
             }
-            if (address instanceof Ipv6Address) {
-                return  LispAFIConvertor.asIPv6AfiAddress(normalizeIP(Inet6Address.getByName(((Ipv6Address) address).getIpv6Address().getValue()),
-                        mask).getHostAddress());
-            }
-
         } catch (UnknownHostException e) {
+            LOG.trace("Failed to normalize " + address + ": " + e.getMessage());
             return null;
         }
         return null;
+    }
+
+    private static LispAFIAddress normalizeIpAddress(LispAFIAddress address, int mask) throws UnknownHostException {
+        if (address instanceof Ipv4Address) {
+            return LispAFIConvertor.asIPAfiAddress(normalizeIP(Inet4Address.getByName(((Ipv4Address) address)
+                    .getIpv4Address().getValue()), mask).getHostAddress());
+        } else if (address instanceof Ipv6Address) {
+            return  LispAFIConvertor.asIPv6AfiAddress(normalizeIP(Inet6Address.getByName(((Ipv6Address) address)
+                    .getIpv6Address().getValue()), mask).getHostAddress());
+        }
+        throw new UnknownHostException("Address + " + address + " not an IPv4 or IPv6 address");
     }
 
     private static InetAddress normalizeIP(InetAddress address, int mask) throws UnknownHostException {
@@ -80,5 +103,4 @@ public class MaskUtil {
             return (byte) -1;
         }
     }
-
 }
