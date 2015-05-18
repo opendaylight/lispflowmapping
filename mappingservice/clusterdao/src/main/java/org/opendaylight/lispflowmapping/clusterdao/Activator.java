64b1dd724a0ab747c60bfb89108c80dbdddffbea
Florin Coras <fcoras@cisco.com>
2015-05-18 16:22:32 -0700

    Removing AD-SAL dependencies
    Change-Id: I8df9f0076d9ed2882f05669c53e6b0ec72b98894
    Signed-off-by: Florin Coras <fcoras@cisco.com>

--- mappingservice/clusterdao/src/main/java/org/opendaylight/lispflowmapping/clusterdao/Activator.java
+++ mappingservice/clusterdao/src/main/java/org/opendaylight/lispflowmapping/clusterdao/Activator.java
@@ -11,10 +11,11 @@ package org.opendaylight.lispflowmapping.clusterdao;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
-import org.apache.felix.dm.Component;
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
 import org.opendaylight.controller.clustering.services.IClusterContainerServices;
-import org.opendaylight.controller.sal.core.ComponentActivatorAbstractBase;
 import org.opendaylight.lispflowmapping.interfaces.dao.ILispDAO;
+import org.osgi.framework.BundleContext;
 
 /**
  * Main application activator class for registering the dependencies and
@@ -22,98 +23,22 @@ import org.opendaylight.lispflowmapping.interfaces.dao.ILispDAO;
  *
  */
 
-public class Activator extends ComponentActivatorAbstractBase {
+public class Activator extends DependencyActivatorBase {
 
-    /**
-     * Function called when the activator starts just after some initializations
-     * are done by the ComponentActivatorAbstractBase.
-     *
-     */
     @Override
-    public void init() {
+    public void init(BundleContext context, DependencyManager manager) throws Exception {
+        Dictionary<String, String> props = new Hashtable<String, String>();
+        props.put("name", "clusterosgiservice");
+        manager.add(createComponent().setInterface(new String[] { ILispDAO.class.getName() }, props)
+                .setImplementation(ClusterDAOService.class)
+                .add(createServiceDependency().setService(IClusterContainerServices.class)
+                        .setCallbacks("setClusterContainerService", "unsetClusterContainerService").setRequired(true)));
+
     }
 
-    /**
-     * Function called when the activator stops just before the cleanup done by
-     * ComponentActivatorAbstractBase
-     *
-     */
     @Override
-    public void destroy() {
-    }
-
-    /**
-     * Function that is used to communicate to dependency manager the list of
-     * known implementations for services inside a container
-     *
-     *
-     * @return An array containing all the CLASS objects that will be
-     *         instantiated in order to get an fully working implementation
-     *         Object
-     */
-    @Override
-    public Object[] getImplementations() {
-        Object[] res = { ClusterDAOService.class };
-        return res;
-    }
-
-    /**
-     * Function that is called when configuration of the dependencies is
-     * required.
-     *
-     * @param c
-     *            dependency manager Component object, used for configuring the
-     *            dependencies exported and imported
-     * @param imp
-     *            Implementation class that is being configured, needed as long
-     *            as the same routine can configure multiple implementations
-     * @param containerName
-     *            The containerName being configured, this allow also optional
-     *            per-container different behavior if needed, usually should not
-     *            be the case though.
-     */
-    @Override
-    public void configureInstance(Component c, Object imp, String containerName) {
-        if (imp.equals(ClusterDAOService.class)) {
-            // export the service
-            Dictionary<String, String> props = new Hashtable<String, String>();
-            props.put("name", "clusterosgiservice");
-            c.setInterface(new String[] { ILispDAO.class.getName() }, props);
-            c.add(createContainerServiceDependency(containerName).setService(IClusterContainerServices.class)
-                    .setCallbacks("setClusterContainerService", "unsetClusterContainerService").setRequired(true));
-        }
-    }
-
-    /**
-     * Method which tells how many Global implementations are supported by the
-     * bundle. This way we can tune the number of components created. This
-     * components will be created ONLY at the time of bundle startup and will be
-     * destroyed only at time of bundle destruction, this is the major
-     * difference with the implementation retrieved via getImplementations where
-     * all of them are assumed to be in a container !
-     *
-     *
-     * @return The list of implementations the bundle will support, in Global
-     *         version
-     */
-    @Override
-    protected Object[] getGlobalImplementations() {
-        return null;
-    }
-
-    /**
-     * Configure the dependency for a given instance Global
-     *
-     * @param c
-     *            Component assigned for this instance, this will be what will
-     *            be used for configuration
-     * @param imp
-     *            implementation to be configured
-     * @param containerName
-     *            container on which the configuration happens
-     */
-    @Override
-    protected void configureGlobalInstance(Component c, Object imp) {
+    public void destroy(BundleContext context, DependencyManager manager) throws Exception {
+        // TODO Auto-generated method stub
 
     }
 }
