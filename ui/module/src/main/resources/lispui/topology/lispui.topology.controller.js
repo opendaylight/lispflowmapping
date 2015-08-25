d55ac0fc73a81071f1e77163ebf4a2bbcaa5d488
Alberto Montes <alm59321@gmail.com>
2015-08-25 10:48:11 +0200

    Add LISP user interface
    Add new menu option LISP UI, with two tabs, Keys and Mappings.
    
    Change-Id: I30f855d686f159e8dd10534a46857f3414d9b470
    Signed-off-by: Alberto Montes <al.montes.gomez@gmail.com>

---
+++ ui/module/src/main/resources/lispui/topology/lispui.topology.controller.js
@@ -0,0 +1,9 @@
+define(['app/lispui/lispui.module', 'app/lispui/lispui.services', 'app/lispui/topology/lispui.topology.services'], function(lispui) {
+
+  lispui.register.controller('TopologyLispuiCtrl', ['$scope',
+    function($scope) {
+
+
+    }
+  ]);
+});
