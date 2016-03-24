7092f6c38c3399d94baa97c2df4abb3490f533a0
Jozef Gloncak <jgloncak@cisco.com>
2016-03-24 10:59:46 +0100

    Integr test - multisite - serie A, test case 2.
    Also refactoring was done.
    
    Change-Id: Ibc5b701858798ff0cb1ed57f9aa57f8bf5d67f2d
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

--- integrationtest/src/test/java/org/opendaylight/lispflowmapping/integrationtest/MultiSiteScenario.java
+++ integrationtest/src/test/java/org/opendaylight/lispflowmapping/integrationtest/MultiSiteScenario.java
@@ -29,7 +29,6 @@ import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.lisp.addres
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.lisp.address.types.rev151105.lisp.address.Address;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.lisp.address.types.rev151105.lisp.address.address.Ipv4;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.MapReply;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.SiteId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.eid.container.Eid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.eid.list.EidItem;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.eid.list.EidItemBuilder;
@@ -49,9 +48,6 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev15090
 
 
 class MultiSiteScenario {
-
-    private final int VNI_2_VALUE = 2;
-    private final InstanceIdType VNI_2 = new InstanceIdType((long) VNI_2_VALUE);
 
     private final int DEFAULT_NETWORK_MASK = 24;
     private final int IP_MASK = 32;
@@ -107,19 +103,17 @@ class MultiSiteScenario {
         return mappingRecord;
     }
 
-    private void emitMapRegisterMessage(final IMappingService mapService, final IFlowMapping lms, final
-    MappingOrigin mappingOrigin, final String destSiteEidPrefix, final String siteRloc, final int vniValue) {
+    private void emitMapRegisterMessage(final MappingOrigin mappingOrigin, final Site dstSite) {
         final MapRegisterBuilder mapRegisterBuilder = new MapRegisterBuilder();
         final MappingRecordItemBuilder mappingRecordItemBuilder = new MappingRecordItemBuilder();
         mappingRecordItemBuilder.setMappingRecordItemId(MAP_RECORD_A);
 
-        final MappingRecordBuilder mrb = prepareMappingRecord(mappingOrigin, siteRloc, null,
-                destSiteEidPrefix, vniValue);
+        final MappingRecordBuilder mrb = prepareMappingRecord(mappingOrigin,  null, dstSite);
         mappingRecordItemBuilder.setMappingRecord(mrb.build());
         mapRegisterBuilder.setMappingRecordItem(Collections.singletonList(mappingRecordItemBuilder.build()));
 
 
-        Eid eid = toEid(destSiteEidPrefix, new InstanceIdType((long) vniValue), DEFAULT_NETWORK_MASK);
+        Eid eid = toEid(dstSite.getEidPrefix(), dstSite.getVNI(), DEFAULT_NETWORK_MASK);
         mapService.addAuthenticationKey(eid, NULL_AUTH_KEY);
         sleepForSeconds(1);
         lms.handleMapRegister(mapRegisterBuilder.build());
@@ -145,50 +139,47 @@ class MultiSiteScenario {
         return lms.handleMapRequest(mapRequestBuilder.build());
     }
 
-    void storeNorthMappingNegative(final Site site, final MappingRecord.Action action) {
-        final Ipv4Prefix ipv4Prefix = new Ipv4Prefix(site.getEidPrefix() + "/" + DEFAULT_NETWORK_MASK);
-        final Eid eidAsIpv4Prefix = LispAddressUtil.toEid(ipv4Prefix, VNI_2);
+    void storeNorthMappingNegative(final Site dstSite, final MappingRecord.Action action) {
+        final Ipv4Prefix ipv4Prefix = new Ipv4Prefix(dstSite.getEidPrefix() + "/" + DEFAULT_NETWORK_MASK);
+        final Eid eidAsIpv4Prefix = LispAddressUtil.toEid(ipv4Prefix, dstSite.getVNI());
 
-        final MappingRecordBuilder mrbNegative = prepareMappingRecord(MappingOrigin.Northbound, null, null, null,
-                VNI_2_VALUE);
+        final MappingRecordBuilder mrbNegative = prepareMappingRecord(MappingOrigin.Northbound, null, dstSite);
         mrbNegative.setEid(eidAsIpv4Prefix);
         mrbNegative.setAction(action);
 
-        mapService.addMapping(MappingOrigin.Northbound, eidAsIpv4Prefix, site.getSiteId(), mrbNegative.build());
+        mapService.addMapping(MappingOrigin.Northbound, eidAsIpv4Prefix, dstSite.getSiteId(), mrbNegative.build());
     }
 
-    private void storeSourceDestinationSiteMappingViaNorthbound(final IMappingService mapService, final int vniValue,
-                                                                final SiteId siteId, final String siteToEidPrefix,
-                                                                final String siteFromEidPrefix, final String siteRloc) {
-        final MappingRecordBuilder mrb = prepareMappingRecord(MappingOrigin.Northbound, siteRloc, siteFromEidPrefix,
-                siteToEidPrefix, vniValue);
-        mapService.addMapping(MappingOrigin.Northbound, mrb.getEid(), siteId, mrb.build());
+    private void storeSourceDestinationSiteMappingViaNorthbound(final Site srcSite, final Site dstSite) {
+        final MappingRecordBuilder mrb = prepareMappingRecord(MappingOrigin.Northbound, srcSite, dstSite);
+        mapService.addMapping(MappingOrigin.Northbound, mrb.getEid(), srcSite.getSiteId(), mrb.build());
     }
 
-    private void storeDestinationSiteMappingViaSouthbound(final int vniValue, final Site site) {
-        emitMapRegisterMessage(mapService, lms, MappingOrigin.Southbound, site.getEidPrefix(), site.getRloc(),
-                vniValue);
+    private void storeDestinationSiteMappingViaSouthbound(final Site dstSite) {
+        emitMapRegisterMessage(MappingOrigin.Southbound, dstSite);
     }
 
-    private MappingRecordBuilder prepareMappingRecord(final MappingOrigin mappingOrigin, final String siteRloc,
-                                                      final String sourceSiteEidPrefix, final String
-                                                              destSiteEidPrefix, final int vniValue) {
+    private MappingRecordBuilder prepareMappingRecord(final MappingOrigin mappingOrigin, final Site srcSite, final
+                                                      Site dstSite) {
         final MappingRecordBuilder mrb = provideCommonMapRecordBuilder();
         Eid eid = null;
         if (MappingOrigin.Northbound.equals(mappingOrigin)) {
-            if (sourceSiteEidPrefix != null && destSiteEidPrefix != null) {
-                eid = LispAddressUtil.asSrcDstEid(sourceSiteEidPrefix, destSiteEidPrefix, DEFAULT_NETWORK_MASK,
-                        DEFAULT_NETWORK_MASK, vniValue);
+            if (srcSite != null && dstSite != null && srcSite.getEidPrefix() != null && dstSite.getEidPrefix() !=
+                    null) {
+                eid = LispAddressUtil.asSrcDstEid(srcSite.getEidPrefix(), dstSite.getEidPrefix(), DEFAULT_NETWORK_MASK,
+                        DEFAULT_NETWORK_MASK, dstSite.getVNI().getValue().intValue());
             }
         } else if (MappingOrigin.Southbound.equals(mappingOrigin)) {
-            if (destSiteEidPrefix != null) {
-                eid = toEid(destSiteEidPrefix, new InstanceIdType((long) vniValue), DEFAULT_NETWORK_MASK);
+            if (dstSite != null && dstSite.getEidPrefix() != null) {
+                eid = toEid(dstSite.getEidPrefix(), dstSite.getVNI(), DEFAULT_NETWORK_MASK);
             }
         }
+
         mrb.setEid(eid);
 
-        if (siteRloc != null) {
-            mrb.setLocatorRecord(provideLocatorRecord(LispAddressUtil.toRloc(new Ipv4Address(siteRloc)), siteRloc));
+        if (dstSite.getRloc() != null) {
+            mrb.setLocatorRecord(provideLocatorRecord(LispAddressUtil.toRloc(new Ipv4Address(dstSite.getRloc())),
+                    dstSite.getRloc()));
         } else {
             mrb.setLocatorRecord(null);
         }
@@ -225,32 +216,33 @@ class MultiSiteScenario {
     }
 
     void storeNorthMappingBidirect(final Site srcSite, final Site dstSite) {
-        storeSourceDestinationSiteMappingViaNorthbound(mapService, VNI_2_VALUE, srcSite.getSiteId(), dstSite
-                .getEidPrefix(), srcSite.getEidPrefix(), dstSite.getRloc());
-        storeSourceDestinationSiteMappingViaNorthbound(mapService, VNI_2_VALUE, dstSite.getSiteId(), srcSite.
-                getEidPrefix(), dstSite.getEidPrefix(), srcSite.getRloc());
+        storeSourceDestinationSiteMappingViaNorthbound(srcSite, dstSite);
+        storeSourceDestinationSiteMappingViaNorthbound(dstSite, srcSite);
     }
 
     void storeSouthboundMapping() {
-        storeDestinationSiteMappingViaSouthbound(VNI_2_VALUE, SITE_A);
-        storeDestinationSiteMappingViaSouthbound(VNI_2_VALUE, SITE_B);
-        storeDestinationSiteMappingViaSouthbound(VNI_2_VALUE, SITE_C);
-        storeDestinationSiteMappingViaSouthbound(VNI_2_VALUE, SITE_D4);
-        storeDestinationSiteMappingViaSouthbound(VNI_2_VALUE, SITE_D5);
+        storeDestinationSiteMappingViaSouthbound(SITE_A);
+        storeDestinationSiteMappingViaSouthbound(SITE_B);
+        storeDestinationSiteMappingViaSouthbound(SITE_C);
+        storeDestinationSiteMappingViaSouthbound(SITE_D4);
+        storeDestinationSiteMappingViaSouthbound(SITE_D5);
     }
 
     void pingBidirect(final Site srcSite, final int srcHostIndex, final Site dstSite, final int dstHostIndex) {
-        pingOneway(srcSite.getHost(srcHostIndex), dstSite.getHost(dstHostIndex), dstSite.getRloc());
-        pingOneway(dstSite.getHost(dstHostIndex), srcSite.getHost(srcHostIndex), srcSite.getRloc());
+        pingOneway(srcSite, srcHostIndex, dstSite, dstHostIndex);
+        pingOneway(dstSite, dstHostIndex, srcSite, srcHostIndex);
     }
 
-    void pingOneway(final String srcIp, final String dstIp, final String dstRloc) {
-        final MapReply mapReplyFromSrcToDst = emitMapRequestMessage(srcIp, dstIp, VNI_2);
-        verifySingleIpv4RlocMapping(mapReplyFromSrcToDst, dstRloc);
+    void pingOneway(final Site srcSite, final int srcHostIndex, final Site dstSite, final int dstHostIndex) {
+        final MapReply mapReplyFromSrcToDst = emitMapRequestMessage(srcSite.getHost(srcHostIndex), dstSite.getHost
+                (dstHostIndex), dstSite.getVNI());
+        verifySingleIpv4RlocMapping(mapReplyFromSrcToDst, dstSite.getRloc());
     }
 
-    void pingOneway(final String srcIp, final String dstIp, final MappingRecord.Action action) {
-        final MapReply mapReplyFromSrcToDst = emitMapRequestMessage(srcIp, dstIp, VNI_2);
+    void pingOneway(final Site srcSite, final int srcHostIndex, final Site dstSite, final int dstHostIndex,  final
+    MappingRecord.Action action) {
+        final MapReply mapReplyFromSrcToDst = emitMapRequestMessage(srcSite.getHost(srcHostIndex), dstSite.getHost
+                        (dstHostIndex), dstSite.getVNI());
         verifySingleIpv4RlocMapping(mapReplyFromSrcToDst, action);
     }
 
