2f560f2ce033c56d5b0bd1d19dad653dc3896a50
Jozef Gloncak <jgloncak@cisco.com>
2016-03-31 15:35:55 +0200

    Checking SMR.
    Adding checks for emiting SMR request.
    
    Change-Id: I25fb5a9b5c13f113daf967d71a0977e3ed76964d
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

--- integrationtest/src/test/java/org/opendaylight/lispflowmapping/integrationtest/MappingServiceIntegrationTest.java
+++ integrationtest/src/test/java/org/opendaylight/lispflowmapping/integrationtest/MappingServiceIntegrationTest.java
@@ -418,23 +418,22 @@ public class MappingServiceIntegrationTest extends AbstractMdsalTestBase {
         multiSiteScenario.assertPingFailsBecauseActionDrop(SITE_D4, 5, SITE_C, 4);
 
         //test case 3
+        resetSocketAndCheckIsEmpty();
         multiSiteScenario.deleteNorthMappingNegative(SITE_C);
         sleepForSeconds(2);
         multiSiteScenario.assertPingWorks(SITE_D4, 5, SITE_C, 4);
 
         //test case 4
-        final DatagramPacket datagramPacket = receivePacket();
+        resetSocketAndCheckIsEmpty();
         multiSiteScenario.storeNorthMappingSrcDst(SITE_B, SITE_C_RLOC_10);
         sleepForSeconds(2);
-        final byte[] data = datagramPacket.getData();
-        assertNotNull(data);
-        MapRequest deserializedMapRequest = MapRequestSerializer.getInstance().deserialize(ByteBuffer.wrap(data));
-        assertTrue(deserializedMapRequest.isSmr());
+        checkSmrPacketIsGenerated();
         //way of testing ping - get RLOC for mapping src-dst and compare it with awaited value doesn't test
         //that ping won't be successfull
         multiSiteScenario.assertPingFails(SITE_B, 5, SITE_C, 4);
 
         //test case 5
+        resetSocketAndCheckIsEmpty();
         multiSiteScenario.storeNorthMappingNegative(SITE_C, Action.Drop);
         sleepForSeconds(2);
         multiSiteScenario.assertPingFails(SITE_D4, 5, SITE_C, 4);
@@ -443,26 +442,51 @@ public class MappingServiceIntegrationTest extends AbstractMdsalTestBase {
         multiSiteScenario.assertPingFails(SITE_D5, 5, SITE_C, 3);
 
         //test case 7
+        resetSocketAndCheckIsEmpty();
         multiSiteScenario.deleteNorthMapingSrcDst(SITE_A, SITE_C);
         multiSiteScenario.storeNorthMappingSrcDst(SITE_B, SITE_C);
         sleepForSeconds(2);
+        checkSmrPacketIsGenerated();
         multiSiteScenario.assertPingWorks(SITE_A, 5, SITE_B, 4);
         multiSiteScenario.assertPingWorks(SITE_B, 5, SITE_C, 4);
         multiSiteScenario.assertPingFails(SITE_A, 1, SITE_C, 4);
 
         //test case 8
+        resetSocketAndCheckIsEmpty();
         multiSiteScenario.deleteNorthMapingSrcDst(SITE_B, SITE_C);
         sleepForSeconds(2);
+        checkSmrPacketIsGenerated();
         multiSiteScenario.assertPingWorks(SITE_A, 5, SITE_B, 4);
         multiSiteScenario.assertPingFails(SITE_B, 5, SITE_C, 4);
         multiSiteScenario.assertPingFails(SITE_A, 1, SITE_C, 4);
 
         //test case 9
+        resetSocketAndCheckIsEmpty();
         multiSiteScenario.deleteNorthMappingNegative(SITE_C);
         sleepForSeconds(2);
+        checkSmrPacketIsGenerated();
         multiSiteScenario.assertPingWorks(SITE_A, 5, SITE_B, 4);
         multiSiteScenario.assertPingWorks(SITE_B, 5, SITE_C, 4);
         multiSiteScenario.assertPingWorks(SITE_A, 5, SITE_C, 4);
+    }
+
+    private void checkSmrPacketIsGenerated() {
+        byte[] data4 = receivePacketAndGetData();
+        assertNotNull(data4);
+        MapRequest deserializedMapRequest = MapRequestSerializer.getInstance().deserialize(ByteBuffer.wrap(data4));
+        assertTrue(deserializedMapRequest.isSmr());
+    }
+
+    private void resetSocketAndCheckIsEmpty() {
+        restartSocket();
+        DatagramPacket datagramPacket;
+        boolean emptySocket = false;
+        try {
+            datagramPacket = receivePacket();
+        } catch (SocketTimeoutException e) {
+            emptySocket = true;
+        }
+        assertTrue(emptySocket);
     }
 
     // ------------------------------- Simple Tests ---------------------------
@@ -1957,6 +1981,15 @@ public class MappingServiceIntegrationTest extends AbstractMdsalTestBase {
         }
     }
 
+    private byte[] receivePacketAndGetData() {
+        try {
+            final DatagramPacket datagramPacket = receivePacket();
+            return datagramPacket.getData();
+        } catch (SocketTimeoutException e) {
+            return null;
+        }
+    }
+
     private DatagramPacket receivePacket() throws SocketTimeoutException {
         return receivePacket(6000);
     }
@@ -2096,6 +2129,11 @@ public class MappingServiceIntegrationTest extends AbstractMdsalTestBase {
         configLispPlugin.shouldListenOnXtrPort(false);
         socket = initSocket(socket, LispMessage.PORT_NUM);
 
+    }
+
+    private void restartSocket() {
+        after();
+        socket = initSocket(socket, LispMessage.PORT_NUM);
     }
 
 }
