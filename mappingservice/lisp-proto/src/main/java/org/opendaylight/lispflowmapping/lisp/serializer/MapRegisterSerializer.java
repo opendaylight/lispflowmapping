2b813cb871c82bfcb07b8645a6b7927f9cf2b8e1
Jozef Gloncak <jgloncak@cisco.com>
2016-04-26 11:20:53 +0200

    WIP: Use cache in map register message.
    Change-Id: I6c5ba8e6ff226d5f4a7b5964159dc138a4bbfc33
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

--- mappingservice/lisp-proto/src/main/java/org/opendaylight/lispflowmapping/lisp/serializer/MapRegisterSerializer.java
+++ mappingservice/lisp-proto/src/main/java/org/opendaylight/lispflowmapping/lisp/serializer/MapRegisterSerializer.java
@@ -7,13 +7,19 @@
  */
 package org.opendaylight.lispflowmapping.lisp.serializer;
 
+import com.google.common.base.Optional;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 import org.apache.commons.lang3.BooleanUtils;
+import org.opendaylight.lispflowmapping.lisp.serializer.cache.MapRegisterCacheKey;
+import org.opendaylight.lispflowmapping.lisp.serializer.cache.MapRegisterCacheKeyBuilder;
+import org.opendaylight.lispflowmapping.lisp.serializer.cache.MapRegisterCacheValue;
+import org.opendaylight.lispflowmapping.lisp.serializer.cache.MapRegisterCacheValueBuilder;
 import org.opendaylight.lispflowmapping.lisp.serializer.exception.LispSerializationException;
 import org.opendaylight.lispflowmapping.lisp.util.ByteUtil;
 import org.opendaylight.lispflowmapping.lisp.util.NumberUtil;
@@ -35,6 +41,17 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.ma
 public final class MapRegisterSerializer {
 
     private static final MapRegisterSerializer INSTANCE = new MapRegisterSerializer();
+    private static final int NUM_FO_BYTES_FROM_START_TO_AUTH_DATA_LENGTH_POS = 13;
+    private static final int NUM_OF_BYTES_FROM_AUTH_DATA_TO_EID_PREFIX_AFI = 10;
+    private static final int NUM_OF_BYTES_EID_PREFIX_AFI = 2;
+    private static final int PAYLOAD1_LEN = 4;
+    private static final int SKIP1_LEN = 8;
+    private static final int PAYLOAD2_LEN = 2;
+    private static final int NUM_OF_BYTES_AUTHENTICATION_DATA_LENGTH = 2;
+    private static final int IPV4 = 1;
+    private static final int IPV6 = 2;
+    private static final int NUM_OF_BYTES_IPV4 = 4;
+    private static final int NUM_OF_BYTES_IPV6 = 16;
 
     // Private constructor prevents instantiation from other classes
     private MapRegisterSerializer() {
@@ -84,6 +101,113 @@ public final class MapRegisterSerializer {
         }
         registerBuffer.clear();
         return registerBuffer;
+    }
+
+
+
+
+    public Map.Entry<MapRegisterCacheKey, MapRegisterCacheValue> deserializePartially(final ByteBuffer buffer,
+                                                                                      final InetAddress sourceRloc) {
+        final int authDataLength = ByteUtil.getUnsignedShort(buffer, NUM_FO_BYTES_FROM_START_TO_AUTH_DATA_LENGTH_POS);
+
+        MapRegisterCacheKey mapRegisterCacheKey = createKeyFromBytes(buffer, authDataLength);
+        if (mapRegisterCacheKey == null) {
+            return null;
+        }
+        MapRegisterCacheValue mapRegisterCacheValue = createValueFromBytes(buffer, authDataLength);
+        if (mapRegisterCacheValue == null) {
+            return null;
+        }
+        return new HashMap.SimpleEntry<>(mapRegisterCacheKey, mapRegisterCacheValue);
+    }
+
+    /**
+     * Extract from buffer the data which doesn't change.
+     *
+     * It means that nonce and authentication data are extracted (skipped)
+     *
+     * payload1 - <Type;RecordCount>
+     * skip1    - Nonce
+     * payload2 - KeyId
+     * skip2    - <Authentication Data Length; Authentication Data>
+     * payload3 - <Record TTL; end of message>
+     *
+     * @param buffer
+     * @return
+     */
+    private MapRegisterCacheValue createValueFromBytes(ByteBuffer buffer, final int authDataLength) {
+        final MapRegisterCacheValueBuilder mapRegisterCacheValueBuilder = new MapRegisterCacheValueBuilder();
+        buffer.position(0);
+        final int bufferLength = buffer.remaining();
+        final int skip2Length = authDataLength + NUM_OF_BYTES_AUTHENTICATION_DATA_LENGTH;
+        final int totalNewPayloadLength = bufferLength - SKIP1_LEN - skip2Length;
+        final byte[] newPayload = new byte[totalNewPayloadLength];
+
+        buffer.get(newPayload, 0, PAYLOAD1_LEN);
+        buffer.position(PAYLOAD1_LEN + SKIP1_LEN);
+        buffer.get(newPayload, PAYLOAD1_LEN, PAYLOAD2_LEN);
+        buffer.position(PAYLOAD1_LEN + SKIP1_LEN + PAYLOAD2_LEN + skip2Length);
+        int payload3Len = totalNewPayloadLength - PAYLOAD1_LEN - PAYLOAD2_LEN;
+        buffer.get(newPayload, PAYLOAD1_LEN + PAYLOAD2_LEN, payload3Len);
+
+        return mapRegisterCacheValueBuilder.setVal(newPayload).build();
+    }
+
+    private MapRegisterCacheKey createKeyFromBytes(final ByteBuffer buffer, int authDataLength) {
+        buffer.mark();
+        byte typeAndFlags = buffer.get();
+        buffer.position(0);
+        boolean xtrSiteIdPresent = ByteUtil.extractBit(typeAndFlags, Flags.XTRSITEID);
+        if (!xtrSiteIdPresent) {
+            return null;
+        }
+        final MapRegisterCacheKeyBuilder mapRegisterCacheKeyBuilder = new MapRegisterCacheKeyBuilder();
+        final byte[] eidPrefix = extractEidPrefix(buffer, authDataLength);
+        if (eidPrefix == null) {
+            return null;
+        }
+        mapRegisterCacheKeyBuilder.setEidPrefix(eidPrefix);
+        if (xtrSiteIdPresent) {
+            buffer.position(0);
+            final int bufferLength = buffer.remaining();
+            buffer.position(bufferLength - Length.XTRID_SIZE - Length.SITEID_SIZE);
+
+            byte[] xtrId  = new byte[Length.XTRID_SIZE];
+            buffer.get(xtrId);
+            mapRegisterCacheKeyBuilder.setXTRId(xtrId);
+
+            byte[] siteId = new byte[Length.SITEID_SIZE];
+            buffer.get(siteId);
+            mapRegisterCacheKeyBuilder.setSiteId(siteId);
+        }
+        return mapRegisterCacheKeyBuilder.build();
+    }
+
+    //TODO: simplification - only one record in map register message is supposed
+    private byte[] extractEidPrefix(final ByteBuffer buffer, int authDataLength) {
+        final int startPositionOfEidPrefixAFI = NUM_FO_BYTES_FROM_START_TO_AUTH_DATA_LENGTH_POS + authDataLength +
+                NUM_OF_BYTES_FROM_AUTH_DATA_TO_EID_PREFIX_AFI;
+        final int eidPrefixAfi = ByteUtil.getUnsignedShort(buffer, startPositionOfEidPrefixAFI);
+        Optional<Integer> eidPrefixLengthOpt = resolveEidPrefixAfi(eidPrefixAfi);
+        if (eidPrefixLengthOpt.isPresent()) {
+            byte[] result = new byte[eidPrefixLengthOpt.get()];
+            final int startPositionOfEidPrefix = startPositionOfEidPrefixAFI + NUM_OF_BYTES_EID_PREFIX_AFI;
+            buffer.position(startPositionOfEidPrefix);
+            buffer.get(result);
+            return result;
+        }
+        return null;
+    }
+
+    private Optional<Integer> resolveEidPrefixAfi(final int eidPrefixAfi) {
+        switch (eidPrefixAfi) {
+            case IPV4:
+                return Optional.of(NUM_OF_BYTES_IPV4);
+            case IPV6:
+                return Optional.of(NUM_OF_BYTES_IPV6);
+            default:
+                return Optional.absent();
+        }
     }
 
     public MapRegister deserialize(ByteBuffer registerBuffer, InetAddress sourceRloc) {
