141f00d79255e05c4eac59496b9dbeba0d398c28
Jozef Gloncak <jgloncak@cisco.com>
2016-04-26 11:20:53 +0200

    Use cache in map register message.
    Map register messages are stored to special cache (hash map).
    
    Key for this has map consists of eid-prefix, site-id and xtr-id.
    Components of key are stored as byte arrays.
    
    Value part of cache entry contains transformed value from key
    (site-id and xtr-id and complete list of eid-prefixes). Additionaly
    there is also timestamp (time of creation or refresh the cache entry),
    want-map-notify value and complete original value of received map
    register message.
    
    Also new type of message mapping-keep-alive is added. This
    notification is raised if map-register message is received and is
    found in cache.
    
    At the end if map-notify-bit is set then also notify message is
    directly generated from southbound.
    
    Change-Id: I6c5ba8e6ff226d5f4a7b5964159dc138a4bbfc33
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

---
+++ mappingservice/southbound/src/main/java/org/opendaylight/lispflowmapping/southbound/lisp/cache/MapRegisterCache.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.lispflowmapping.southbound.lisp.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.map.register.cache.key.container.MapRegisterCacheKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.map.register.cache.metadata.container.MapRegisterCacheMetadata;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.map.register.cache.metadata.container.MapRegisterCacheMetadataBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.map.register.cache.value.grouping.MapRegisterCacheValue;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.map.register.cache.value.grouping.MapRegisterCacheValueBuilder;
+
+public class MapRegisterCache {
+
+    protected final Map<MapRegisterCacheKey, MapRegisterCacheValue> cache;
+
+    public MapRegisterCache() {
+        cache = new ConcurrentHashMap<>();
+    }
+
+    public void addEntry(final MapRegisterCacheKey mapRegisterCacheKey, final MapRegisterCacheValue
+            mapRegisterCacheValue) {
+        cache.put(mapRegisterCacheKey, mapRegisterCacheValue);
+    }
+
+    public void addEntry(final Map.Entry<MapRegisterCacheKey, MapRegisterCacheValue>
+                                 mapRegisterCacheEntry) {
+        cache.put(mapRegisterCacheEntry.getKey(), mapRegisterCacheEntry.getValue());
+    }
+
+    public MapRegisterCacheValue getEntry(final MapRegisterCacheKey mapRegisterCacheKey) {
+        if (mapRegisterCacheKey != null) {
+            return cache.get(mapRegisterCacheKey);
+        }
+        return null;
+    }
+
+    public void removeEntry(final MapRegisterCacheKey mapRegisterCacheKey) {
+        if (mapRegisterCacheKey != null) {
+            cache.remove(mapRegisterCacheKey);
+        }
+    }
+
+    public void refreshEntry(final MapRegisterCacheKey mapRegisterCacheKey) {
+        final MapRegisterCacheValue mapRegisterCacheValueOld = cache.get(mapRegisterCacheKey);
+        final MapRegisterCacheMetadata mapRegisterCacheMetadataOld = mapRegisterCacheValueOld
+                .getMapRegisterCacheMetadata();
+
+        final MapRegisterCacheMetadataBuilder mapRegisterCacheMetadataBuilderNew = new MapRegisterCacheMetadataBuilder
+                (mapRegisterCacheMetadataOld);
+        mapRegisterCacheMetadataBuilderNew.setTimestamp(System.currentTimeMillis());
+
+        final MapRegisterCacheValueBuilder mapRegisterCacheValueBuilderNew = new MapRegisterCacheValueBuilder();
+        mapRegisterCacheValueBuilderNew.setPacketData(mapRegisterCacheValueOld.getPacketData());
+        mapRegisterCacheValueBuilderNew.setMapRegisterCacheMetadata(mapRegisterCacheMetadataBuilderNew.build());
+
+        cache.put(mapRegisterCacheKey, mapRegisterCacheValueBuilderNew.build());
+    }
+
+    public int cacheSize() {
+        return cache.size();
+    }
+}
