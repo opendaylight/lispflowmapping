4127c35e58242d6cb5870c2d2c9879e364fa90e5
Jozef Gloncak <jgloncak@cisco.com>
2016-04-26 11:20:53 +0200

    WIP: Use cache in map register message.
    <a href=https://trello.com/c/IXhTWoVe/29-performance-use-a-local-cache-for-map-registers-in-byte-form-in-sb-and-only-update-map-cache-if-necessary>link to trello</a>
    
    Change-Id: I6c5ba8e6ff226d5f4a7b5964159dc138a4bbfc33
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

---
+++ mappingservice/lisp-proto/src/main/java/org/opendaylight/lispflowmapping/lisp/serializer/cache/MapRegisterCache.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.lispflowmapping.lisp.serializer.cache;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.opendaylight.yang.gen.v1.mapregister.cache.rev160425.MapRegisterCacheKey;
+import org.opendaylight.yang.gen.v1.mapregister.cache.rev160425.MapRegisterCacheValue;
+
+public class MapRegisterCache {
+    private final Map<MapRegisterCacheKey, MapRegisterCacheValue> cache;
+
+
+    public MapRegisterCache() {
+        cache = new HashMap<>();
+    }
+
+    public void addMapRegisterEntryToCache(final MapRegisterCacheKey mapRegisterCacheKey, final MapRegisterCacheValue
+            mapRegisterCacheValue) {
+        cache.put(mapRegisterCacheKey, mapRegisterCacheValue);
+    }
+
+    public void addMapRegisterEntryToCache(final Map.Entry<MapRegisterCacheKey,MapRegisterCacheValue>
+                                                   mapRegisterCacheEntry) {
+        cache.put(mapRegisterCacheEntry.getKey(), mapRegisterCacheEntry.getValue());
+    }
+
+    public boolean isMapRegisterKeyInCache(final MapRegisterCacheKey mapRegisterCacheKey) {
+        return cache.get(mapRegisterCacheKey) != null ? true : false;
+    }
+}
