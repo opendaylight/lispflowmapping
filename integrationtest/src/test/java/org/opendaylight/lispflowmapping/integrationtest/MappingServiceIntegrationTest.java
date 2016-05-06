8883cf4acfb8b391dcc972c9dd6d21f4168fcdaa
Lorand Jakab <lojakab@cisco.com>
2016-05-06 12:11:20 +0300

    YANG model for the Map-Register cache
    Change-Id: Ib2a5171731347ef600d09ddcc37af14c48e0deee
    Signed-off-by: Lorand Jakab <lojakab@cisco.com>

--- integrationtest/src/test/java/org/opendaylight/lispflowmapping/integrationtest/MappingServiceIntegrationTest.java
+++ integrationtest/src/test/java/org/opendaylight/lispflowmapping/integrationtest/MappingServiceIntegrationTest.java
@@ -45,6 +45,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+
 //import org.codehaus.jettison.json.JSONException;
 //import org.codehaus.jettison.json.JSONObject;
 //import org.codehaus.jettison.json.JSONTokener;
@@ -72,6 +73,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.Ma
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.MapRegister;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.MapReply;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.MapRequest;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.MappingKeepAlive;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.OdlLispProtoListener;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.RequestMapping;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.XtrReplyMapping;
@@ -1987,6 +1989,10 @@ public class MappingServiceIntegrationTest extends AbstractMdsalTestBase {
 
         @Override
         public void onXtrRequestMapping(XtrRequestMapping notification) {
+        }
+
+        @Override
+        public void onMappingKeepAlive(MappingKeepAlive notification) {
         }
 
     }
