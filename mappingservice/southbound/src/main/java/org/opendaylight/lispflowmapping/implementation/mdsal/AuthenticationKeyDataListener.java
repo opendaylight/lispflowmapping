aaab31d94b3bd4d6b02397e14dd4c9b9d7d975c8
Jozef Gloncak <jgloncak@cisco.com>
2016-05-19 14:21:54 +0200

    Removing AbstractDataListener.
    AbstractDataListener is removed from codebase.
    Methods from abstract class were added to child MappingDataListener.
    The same with class AuthenticationKeyDataListener which was
    also moved to southbound.
    
    Change-Id: I5e6fc7c137704bab86987224351975ae9e0e8359
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

--- mappingservice/southbound/src/main/java/org/opendaylight/lispflowmapping/implementation/mdsal/AuthenticationKeyDataListener.java
+++ mappingservice/southbound/src/main/java/org/opendaylight/lispflowmapping/implementation/mdsal/AuthenticationKeyDataListener.java
@@ -10,15 +10,19 @@ package org.opendaylight.lispflowmapping.implementation.mdsal;
 import java.util.Collection;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataObjectModification;
+import org.opendaylight.controller.md.sal.binding.api.DataObjectModification.ModificationType;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeChangeListener;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
-import org.opendaylight.lispflowmapping.interfaces.mapcache.IMappingSystem;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.lispflowmapping.mapcache.SimpleMapCache;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev150906.MappingDatabase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev150906.db.instance.AuthenticationKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev150906.mapping.database.VirtualNetworkIdentifier;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.opendaylight.controller.md.sal.binding.api.DataObjectModification.ModificationType;
 
 /**
  * DataListener for all AuthenticationKey modification events.
@@ -26,17 +30,30 @@ import org.opendaylight.controller.md.sal.binding.api.DataObjectModification.Mod
  * @author Lorand Jakab
  *
  */
-public class AuthenticationKeyDataListener extends AbstractDataListener<AuthenticationKey> {
-    private static final Logger LOG = LoggerFactory.getLogger(AuthenticationKeyDataListener.class);
-    private IMappingSystem mapSystem;
 
-    public AuthenticationKeyDataListener(DataBroker broker, IMappingSystem mapSystem) {
-        setBroker(broker);
-        setMappingSystem(mapSystem);
-        setPath(InstanceIdentifier.create(MappingDatabase.class).child(VirtualNetworkIdentifier.class)
-                .child(AuthenticationKey.class));
+
+public class AuthenticationKeyDataListener implements DataTreeChangeListener<AuthenticationKey> {
+    private static final Logger LOG = LoggerFactory.getLogger(AuthenticationKeyDataListener.class);
+
+    private final SimpleMapCache smc;
+    private final DataBroker broker;
+    private final InstanceIdentifier<AuthenticationKey> path;
+    private ListenerRegistration<DataTreeChangeListener<AuthenticationKey>> registration;
+
+
+    public AuthenticationKeyDataListener(final DataBroker broker, final SimpleMapCache smc) {
+        this.broker = broker;
+        this.smc = smc;
+        this.path = InstanceIdentifier.create(MappingDatabase.class).child(VirtualNetworkIdentifier.class)
+                .child(AuthenticationKey.class);
         LOG.trace("Registering AuthenticationKey listener.");
-        registerDataChangeListener();
+        final DataTreeIdentifier<AuthenticationKey> dataTreeIdentifier = new DataTreeIdentifier<>
+                (LogicalDatastoreType.CONFIGURATION, path);
+        registration = broker.registerDataTreeChangeListener(dataTreeIdentifier, this);
+    }
+
+    public void closeDataChangeListener() {
+        registration.close();
     }
 
     @Override
@@ -51,7 +68,7 @@ public class AuthenticationKeyDataListener extends AbstractDataListener<Authenti
                 LOG.trace("Key: {}", change.getRootPath().getRootIdentifier());
                 LOG.trace("Value: {}", authKey);
 
-                mapSystem.removeAuthenticationKey(authKey.getEid());
+                smc.removeAuthenticationKey(authKey.getEid());
             } else if (ModificationType.WRITE == mod.getModificationType() || ModificationType.SUBTREE_MODIFIED == mod
                     .getModificationType()) {
                 if (ModificationType.WRITE == mod.getModificationType()) {
@@ -65,14 +82,11 @@ public class AuthenticationKeyDataListener extends AbstractDataListener<Authenti
                 LOG.trace("Key: {}", change.getRootPath().getRootIdentifier());
                 LOG.trace("Value: {}", authKey);
 
-                mapSystem.addAuthenticationKey(authKey.getEid(), authKey.getMappingAuthkey());
+                smc.addAuthenticationKey(authKey.getEid(), authKey.getMappingAuthkey());
             } else {
                 LOG.warn("Ignoring unhandled modification type {}", mod.getModificationType());
             }
         }
     }
 
-    void setMappingSystem(IMappingSystem msmr) {
-        this.mapSystem = msmr;
-    }
 }
