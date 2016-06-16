685eb45db9902e3eb97f7eb60af451d754ab3d33
Jozef Gloncak <jgloncak@cisco.com>
2016-06-16 15:42:45 +0200

    Clustering - common infrastructure.
    Common class for clustering operations for each module.
    
    Change-Id: If401eba45155e07846f025340b6499969213e7bb
    Signed-off-by: Jozef Gloncak <jgloncak@cisco.com>

---
+++ mappingservice/clustering/src/main/java/org/opendaylight/lispflowmapping/clustering/util/ClusteringUtil.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.lispflowmapping.clustering.util;
+
+public final class ClusteringUtil {
+
+    public static final String LISPFLOWMAPPING_ENTITY_NAME = "lispflowmapping";
+    public static final String LISPFLOWMAPPING_ENTITY_TYPE = "application";
+
+    private ClusteringUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+}
