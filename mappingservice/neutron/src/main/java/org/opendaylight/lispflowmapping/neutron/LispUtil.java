44fd68054f0af3213c65f944177a796f4047af17
Miroslav Toth <mirtoth@cisco.com>
2016-07-13 13:53:30 +0200

    Reenable Neutron using YANG models
    Change-Id: I777875bff02827aa467278d82cbe15ae7b453d20
    Signed-off-by: Miroslav Toth <mirtoth@cisco.com>

--- mappingservice/neutron/src/main/java/org/opendaylight/lispflowmapping/neutron/LispUtil.java
+++ mappingservice/neutron/src/main/java/org/opendaylight/lispflowmapping/neutron/LispUtil.java
@@ -5,15 +5,17 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.lispflowmapping.neutron;
 
 import java.util.List;
-
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.eid.container.Eid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.locatorrecords.LocatorRecord;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.mapping.record.container.MappingRecord.Action;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.mapping.record.container.MappingRecordBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.mapping.authkey.container
+        .MappingAuthkeyBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.mapping.record.container.MappingRecord
+        .Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.mapping.record.container
+        .MappingRecordBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev150906.AddKeyInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev150906.AddKeyInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev150906.AddMappingInput;
@@ -24,16 +26,10 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev15090
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev150906.RemoveKeyInputBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev150906.RemoveMappingInput;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev150906.RemoveMappingInputBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.mapping.authkey.container.MappingAuthkeyBuilder;
 
 /**
  * LispUtil class has util functions for building inputs for LISP service RPCs.
- *
- *
- *
  */
-
-
 public final class LispUtil {
     // Utility class, should not be instantiated
     private LispUtil() {
@@ -70,6 +66,7 @@ public final class LispUtil {
         rmib.setEid(eid);
         return rmib.build();
     }
+
     public static RemoveKeyInput buildRemoveKeyInput(Eid eid) {
         RemoveKeyInputBuilder kib = new RemoveKeyInputBuilder();
 
