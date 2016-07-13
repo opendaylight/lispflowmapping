f7e1d72d2644c893fdaf79cf01f48803177f0315
Miroslav Toth <mirtoth@cisco.com>
2016-07-13 13:53:30 +0200

    WIP: Reenable Neutron using YANG models
    Change-Id: I777875bff02827aa467278d82cbe15ae7b453d20
    Signed-off-by: Miroslav Toth <mirtoth@cisco.com>

---
+++ mappingservice/neutron/src/main/java/org/opendaylight/lispflowmapping/neutron/SubnetListener.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.lispflowmapping.neutron;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.subnets.rev150712.subnets.attributes.Subnets;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.subnets.rev150712.subnets.attributes.subnets.Subnet;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class SubnetListener extends DelegatingDataTreeListener<Subnet> {
+
+    private static final DataTreeIdentifier<Subnet> IDENTIFIER =
+            new DataTreeIdentifier<>(LogicalDatastoreType.CONFIGURATION,
+                    InstanceIdentifier.create(Neutron.class).child(Subnets.class).child(Subnet.class));
+
+    public SubnetListener(DataProcessor<Subnet> dataProcessor, DataBroker dataBroker) {
+        super(dataProcessor, dataBroker, IDENTIFIER);
+    }
+
+    @Override
+    public String toString() {
+        return "SubnetListener";
+    }
+}
