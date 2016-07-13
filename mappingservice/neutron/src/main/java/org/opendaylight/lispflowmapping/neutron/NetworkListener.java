32037f80b38b13aa0c3b89ce759ecb9d249a7b3c
Miroslav Toth <mirtoth@cisco.com>
2016-07-13 13:53:30 +0200

    Reenable Neutron using YANG models
    Change-Id: I777875bff02827aa467278d82cbe15ae7b453d20
    Signed-off-by: Miroslav Toth <mirtoth@cisco.com>

---
+++ mappingservice/neutron/src/main/java/org/opendaylight/lispflowmapping/neutron/NetworkListener.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.lispflowmapping.neutron;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.networks.rev150712.networks.attributes.Networks;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.networks.rev150712.networks.attributes.networks.Network;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.neutron.rev150712.Neutron;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * Implementation of a ClusteredDataTreeChangeListener that listens for northbound requests on a {@link Network}
+ * subtree.
+ */
+public class NetworkListener extends DelegatingDataTreeListener<Network> {
+
+    private static final DataTreeIdentifier<Network> IDENTIFIER =
+            new DataTreeIdentifier<>(LogicalDatastoreType.CONFIGURATION,
+                    InstanceIdentifier.create(Neutron.class).child(Networks.class).child(Network.class));
+
+    public NetworkListener(DataProcessor<Network> dataProcessor, DataBroker dataBroker) {
+        super(dataProcessor, dataBroker, IDENTIFIER);
+    }
+
+    @Override
+    public String toString() {
+        return "NetworkListener";
+    }
+}
