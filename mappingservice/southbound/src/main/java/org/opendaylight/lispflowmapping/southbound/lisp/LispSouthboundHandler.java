6b699b78ee9dcade96adf36555e45798132aaff8
Lorand Jakab <lojakab@cisco.com>
2016-08-03 11:59:08 -0500

    Revert "Clustering - adding to LispSouthboundPlugin."
    This reverts commit c174d5b216d6824714480c8e40a9d1655022c1f7.
    
    The reason is to see if it was causing a performance regression (as
    measured by Jenkins).
    
    Change-Id: Ifa8bb37739fc65816f4658178077542800fb80b9
    Signed-off-by: Lorand Jakab <lojakab@cisco.com>

--- mappingservice/southbound/src/main/java/org/opendaylight/lispflowmapping/southbound/lisp/LispSouthboundHandler.java
+++ mappingservice/southbound/src/main/java/org/opendaylight/lispflowmapping/southbound/lisp/LispSouthboundHandler.java
@@ -94,7 +94,6 @@ public class LispSouthboundHandler extends SimpleChannelInboundHandler<DatagramP
     private SimpleMapCache smc;
     private AuthenticationKeyDataListener authenticationKeyDataListener;
     private DataStoreBackEnd dsbe;
-    private boolean isReadFromChannelEnabled = true;
 
     public LispSouthboundHandler(LispSouthboundPlugin lispSbPlugin) {
         this.lispSbPlugin = lispSbPlugin;
@@ -499,13 +498,11 @@ public class LispSouthboundHandler extends SimpleChannelInboundHandler<DatagramP
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
-        if (isReadFromChannelEnabled) {
-            if (LOG.isTraceEnabled()) {
-                LOG.trace("Received UDP packet from {}:{} with content:\n{}", msg.sender().getHostString(),
-                        msg.sender().getPort(), ByteBufUtil.prettyHexDump(msg.content()));
-            }
-            handlePacket(msg);
+        if (LOG.isTraceEnabled()) {
+            LOG.trace("Received UDP packet from {}:{} with content:\n{}", msg.sender().getHostString(),
+                    msg.sender().getPort(), ByteBufUtil.prettyHexDump(msg.content()));
         }
+        handlePacket(msg);
     }
 
     @Override
@@ -565,10 +562,6 @@ public class LispSouthboundHandler extends SimpleChannelInboundHandler<DatagramP
         Preconditions.checkNotNull(smc);
         this.authenticationKeyDataListener = new AuthenticationKeyDataListener(dataBroker, smc);
         dsbe = new DataStoreBackEnd(dataBroker);
-    }
-
-    public void setIsReadFromChannelEnabled(boolean isReadFromChannelEnabled) {
-        this.isReadFromChannelEnabled = isReadFromChannelEnabled;
     }
 
     public void setMapRegisterCacheTimeout(long mapRegisterCacheTimeout) {
