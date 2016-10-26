aac90a2070c406651b400334baa12bdfe60464ac
Miroslav Toth <mirtoth@cisco.com>
2016-10-26 15:11:26 +0200

    WIP: Reliable SMR - fix performance issue
    Change-Id: I7f942037002cb983432368f54fd32c260b838e91
    Signed-off-by: Miroslav Toth <mirtoth@cisco.com>

--- mappingservice/api/src/main/java/org/opendaylight/lispflowmapping/interfaces/lisp/SmrEvent.java
+++ mappingservice/api/src/main/java/org/opendaylight/lispflowmapping/interfaces/lisp/SmrEvent.java
@@ -9,6 +9,7 @@ package org.opendaylight.lispflowmapping.interfaces.lisp;
 
 import java.util.List;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.inet.binary.types.rev160303.IpAddressBinary;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.eid.container.Eid;
 
 /**
  * Carries information about received SMR-invoked request.
@@ -16,12 +17,39 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.inet.binary.types.rev16
 public class SmrEvent {
 
     private final List<IpAddressBinary> subscriberAddresses;
+    private final Eid eid;
+    private final long nonce;
 
-    public SmrEvent(List<IpAddressBinary> subscriberAddresses) {
+    public SmrEvent(List<IpAddressBinary> subscriberAddresses, Eid eid, long nonce) {
         this.subscriberAddresses = subscriberAddresses;
+        this.eid = eid;
+        this.nonce = nonce;
     }
 
+    /**
+     * Returns the list of subscriber addresses that are subscribed to receive SMR MapRequest for a specific eid.
+     *
+     * @return the list of subscriber Addresses.
+     */
     public List<IpAddressBinary> getSubscriberAddressList() {
         return subscriberAddresses;
+    }
+
+    /**
+     * Returns the eid which the xTRs are subscribed to.
+     *
+     * @return the subscribed eid.
+     */
+    public Eid getEid() {
+        return eid;
+    }
+
+    /**
+     * Returns the nonce associated to a MapRequest.
+     *
+     * @return the nonce.
+     */
+    public long getNonce() {
+        return nonce;
     }
 }
