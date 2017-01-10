21953d93c4b286c52dd3a98981e4cae377a32ee1
Shakib Ahmed <sheikahm@cisco.com>
2017-01-10 14:17:54 -0800

    Add Neutron host-id to RLOC mapping
    Neutron port create call provides us informations like port uuid, host-
    id, sometimes fixed-ips but we don't know the interface VPP is going to
    use for sending traffic from one host to another. That interface is
    going to be the locator for the mapping record we will be putting in
    the map-cache. For that, we need to know the locator interface of a
    host. Just to clarify, for OpenStack environment is going to be the
    tenant network interface.
    
    This patch adds the functionality of listening to VbridgeTopology for
    topology updates in Virtual Bridge Domain and pre-populate host to
    locator information base.
    
    Change-Id: Ic4c0844ccfcb972e46122df875b628c789f8c837
    Signed-off-by: Shakib Ahmed <sheikahm@cisco.com>

---
+++ mappingservice/neutron/src/main/java/org/opendaylight/lispflowmapping/neutron/intenthandler/IntentHandlerAsyncExecutorProvider.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.lispflowmapping.neutron.intenthandler;
+
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+
+import java.util.concurrent.Executors;
+
+/**
+ * Created by Shakib Ahmed on 1/24/17.
+ */
+public class IntentHandlerAsyncExecutorProvider {
+    private static ListeningExecutorService listeningExecutorService;
+
+    private static final int EXTRA_THREADS_TO_HANDLE_VPP_LISTENER = 1;
+
+    private static IntentHandlerAsyncExecutorProvider intentHandlerAsyncExecutorProvider;
+
+    private IntentHandlerAsyncExecutorProvider() {
+        listeningExecutorService = MoreExecutors
+                .listeningDecorator(Executors.newFixedThreadPool(EXTRA_THREADS_TO_HANDLE_VPP_LISTENER));
+    }
+
+    public static synchronized IntentHandlerAsyncExecutorProvider getInstace() {
+        if (intentHandlerAsyncExecutorProvider == null) {
+            intentHandlerAsyncExecutorProvider = new IntentHandlerAsyncExecutorProvider();
+        }
+        return intentHandlerAsyncExecutorProvider;
+    }
+
+    public synchronized ListeningExecutorService getExecutor() {
+        return listeningExecutorService;
+    }
+
+    public void close() {
+        listeningExecutorService.shutdown();
+    }
+}
---
+++ mappingservice/neutron/src/main/java/org/opendaylight/lispflowmapping/neutron/intenthandler/exception/RlocNotFoundOnVppNode.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.lispflowmapping.neutron.intenthandler.exception;
+
+/**
+ * Created by Shakib Ahmed on 1/12/17.
+ */
+public class RlocNotFoundOnVppNode extends RuntimeException {
+    public RlocNotFoundOnVppNode(String hostId) {
+        super("No available interface found on node " + hostId);
+    }
+}
