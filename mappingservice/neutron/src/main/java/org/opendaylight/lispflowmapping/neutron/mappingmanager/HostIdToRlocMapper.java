6c2cdb390b468bf6f71b44908e58530c63e3234d
Shakib Ahmed <sheikahm@cisco.com>
2017-01-10 14:17:54 -0800

    Add Neutron host-id to RLOC mapping
    Neutron port create call provides us informations like port uuid, host-
    id, sometimes fixed-ips but we don't know the interface VPP is going to
    use for sending traffic from one host to another. That interface is
    going to be the locator for the mapping record we will be putting in
    the map-cache. For that, we need to know the locator interface of a
    host. Just to clarify, for OpenStack environment is going to be the
    tenant network interface.
    
    This patch adds the functionality of listening to VbridgeTopology for
    topology updates in Virtual Bridge Domain and pre-populate host to
    locator information base.
    
    Change-Id: Ic4c0844ccfcb972e46122df875b628c789f8c837
    Signed-off-by: Shakib Ahmed <sheikahm@cisco.com>

---
+++ mappingservice/neutron/src/main/java/org/opendaylight/lispflowmapping/neutron/mappingmanager/HostIdToRlocMapper.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.lispflowmapping.neutron.mappingmanager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.rloc.container.Rloc;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Shakib Ahmed on 1/12/17.
+ */
+public class HostIdToRlocMapper {
+    private static final Logger LOG = LoggerFactory.getLogger(HostIdToRlocMapper.class);
+
+    private ConcurrentHashMap<String, List<Rloc>> mapper;
+    private static HostIdToRlocMapper instance;
+
+    private HostIdToRlocMapper() {
+        mapper = new ConcurrentHashMap();
+    }
+
+    public static synchronized HostIdToRlocMapper getInstance() {
+        if (instance == null) {
+            instance = new HostIdToRlocMapper();
+        }
+        return instance;
+    }
+
+    public synchronized void addMapping(String hostId, Rloc hostRloc) {
+        List<Rloc> hostIdSpeceficRlocs = mapper.get(hostId);
+
+        if (hostIdSpeceficRlocs == null) {
+            hostIdSpeceficRlocs = new ArrayList<>();
+        }
+
+        hostIdSpeceficRlocs.add(hostRloc);
+        mapper.put(hostId, hostIdSpeceficRlocs);
+        LOG.debug("Adding " + hostRloc.getAddress() + " as Rloc of " + hostId);
+    }
+
+    public List<Rloc> getRloc(String hostId) {
+        return mapper.get(hostId);
+    }
+
+    public synchronized void deleteMapping(String hostId) {
+        //for now, delete all rlocs
+        mapper.remove(hostId);
+    }
+}
