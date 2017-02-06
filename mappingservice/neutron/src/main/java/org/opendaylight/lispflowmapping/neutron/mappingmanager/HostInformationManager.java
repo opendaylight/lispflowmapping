c6366be79ee31e23acb0ba57d6c2d2e35b0acd98
Shakib Ahmed <sheikahm@cisco.com>
2017-02-05 16:14:17 -0800

    Add mapping record with port data add with fixed ip
    When a neutron port create comes with a fixed ip, we consider the fixed
    ip as EID. We need to save this EIDs specific to host id, so that when
    we have host id to locator mapping available we can create a mapping
    record.
    
    This patch adds hostid to all eid mappings keyed by port uuid. And also
    attempts to create a mapping record if hostid to locator mapping is
    available.
    
    Change-Id: Ia6ae4643e1ecf5d16c592bcd9a52170b28f38777
    Signed-off-by: Shakib Ahmed <sheikahm@cisco.com>

---
+++ mappingservice/neutron/src/main/java/org/opendaylight/lispflowmapping/neutron/mappingmanager/HostInformationManager.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.lispflowmapping.neutron.mappingmanager;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.opendaylight.lispflowmapping.lisp.util.LispAddressUtil;
+import org.opendaylight.lispflowmapping.neutron.LispUtil;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.locatorrecords.LocatorRecord;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.rloc.container.Rloc;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.mappingservice.rev150906.OdlMappingserviceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by Shakib Ahmed on 2/5/17.
+ */
+public class HostInformationManager {
+    private static final Logger LOG = LoggerFactory.getLogger(HostInformationManager.class);
+
+    private OdlMappingserviceService lfmDbService;
+    private HostIdToPortDataMapper hostIdToPortDataMapper;
+    private HostIdToRlocMapper hostIdToRlocMapper;
+
+    private static HostInformationManager instance;
+
+    private HostInformationManager() {
+        hostIdToPortDataMapper = new HostIdToPortDataMapper();
+        hostIdToRlocMapper = new HostIdToRlocMapper();
+    }
+
+    public static synchronized HostInformationManager getInstance() {
+        if (instance == null) {
+            instance = new HostInformationManager();
+        }
+        return instance;
+    }
+
+    public void setOdlMappingserviceService(OdlMappingserviceService lfmDbService) {
+        this.lfmDbService = lfmDbService;
+    }
+
+    public synchronized void addHostRelatedInfo(String hostId, Object data) {
+        if (data instanceof PortData) {
+            hostIdToPortDataMapper.addMapping(hostId, (PortData) data);
+
+        } else if (data instanceof Rloc) {
+            hostIdToRlocMapper.addMapping(hostId, (Rloc) data);
+        } else {
+            LOG.debug("Unwanted information type " + data.getClass().getSimpleName()
+                    + " for Host Id " + hostId);
+        }
+
+        attemptToCreateMappingRecord(hostId);
+    }
+
+    private synchronized void attemptToCreateMappingRecord(String hostId) {
+        List<Rloc> rlocForMappingRecord = hostIdToRlocMapper.getRlocs(hostId);
+        PortUuidToPortDataMapper uuidToEidMapper = hostIdToPortDataMapper.getAllPortData(hostId);
+
+        if (rlocForMappingRecord == null || rlocForMappingRecord.isEmpty()
+                || uuidToEidMapper == null) {
+            return;
+        }
+
+        Collection<PortData> allUnprocessedPorts = uuidToEidMapper.getAllUnprocessedPorts();
+        List<LocatorRecord> locators = LispAddressUtil.asLocatorRecords(rlocForMappingRecord);
+
+        allUnprocessedPorts.forEach(portData -> {
+            lfmDbService.addMapping(LispUtil.buildAddMappingInput(portData.getPortEid(), locators));
+            uuidToEidMapper.addPortDataToProcessed(portData);
+        });
+        uuidToEidMapper.clearAllUnprocessedData();
+
+    }
+}
---
+++ mappingservice/neutron/src/test/java/org/opendaylight/lispflowmapping/neutron/mappingmanager/PortUuidToPortDataMapperTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2017 Cisco Systems, Inc.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.lispflowmapping.neutron.mappingmanager;
+
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.eid.container.Eid;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.powermock.reflect.Whitebox;
+
+/**
+ * Created by Shakib Ahmed on 2/7/17.
+ */
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(MockitoJUnitRunner.class)
+@PrepareForTest(PortUuidToPortDataMapper.class)
+public class PortUuidToPortDataMapperTest {
+
+    private static final Eid SAMPLE_EID = Mockito.mock(Eid.class);
+    private static final PortData PORT_DATA_1 = new PortData("1", SAMPLE_EID);
+    private static final PortData PORT_DATA_2 = new PortData("2", SAMPLE_EID);
+    private static final PortData PORT_DATA_3 = new PortData("3", SAMPLE_EID);
+    private static final PortData PORT_DATA_4 = new PortData("4", SAMPLE_EID);
+
+    @Test
+    public void unprocessedPortDataAddTest() {
+        PortUuidToPortDataMapper portUuidToPortDataMapper = new PortUuidToPortDataMapper();
+
+        portUuidToPortDataMapper.addUnprocessedUuidToPortDataMapping(PORT_DATA_1.getPortUuid(), PORT_DATA_1);
+        portUuidToPortDataMapper.addUnprocessedUuidToPortDataMapping(PORT_DATA_2.getPortUuid(), PORT_DATA_2);
+
+        processedAndUnprocessedDataAmountVerify(portUuidToPortDataMapper, 0, 2);
+    }
+
+    @Test
+    public void addPortDataToProcessedTest() {
+        PortUuidToPortDataMapper portUuidToPortDataMapper = new PortUuidToPortDataMapper();
+
+        portUuidToPortDataMapper.addPortDataToProcessed(PORT_DATA_1);
+        portUuidToPortDataMapper.addPortDataToProcessed(PORT_DATA_2);
+
+        processedAndUnprocessedDataAmountVerify(portUuidToPortDataMapper, 2, 0);
+    }
+
+    @Test
+    public void transferAllProcessedPortDataToUnprocessedTest() {
+        PortUuidToPortDataMapper portUuidToPortDataMapper = new PortUuidToPortDataMapper();
+
+        portUuidToPortDataMapper.addUnprocessedUuidToPortDataMapping(PORT_DATA_3.getPortUuid(), PORT_DATA_3);
+        portUuidToPortDataMapper.addUnprocessedUuidToPortDataMapping(PORT_DATA_4.getPortUuid(), PORT_DATA_4);
+
+        portUuidToPortDataMapper.addPortDataToProcessed(PORT_DATA_1);
+        portUuidToPortDataMapper.addPortDataToProcessed(PORT_DATA_2);
+
+        portUuidToPortDataMapper.transferAllProcessedPortDataToUnprocessed();
+
+        processedAndUnprocessedDataAmountVerify(portUuidToPortDataMapper, 0, 4);
+
+        HashMap<String, PortData> unprocessedPortData = extractUnprocessedDataMap(portUuidToPortDataMapper);
+
+        Assert.assertEquals(PORT_DATA_1, unprocessedPortData.get(PORT_DATA_1.getPortUuid()));
+        Assert.assertEquals(PORT_DATA_2, unprocessedPortData.get(PORT_DATA_2.getPortUuid()));
+        Assert.assertEquals(PORT_DATA_3, unprocessedPortData.get(PORT_DATA_3.getPortUuid()));
+        Assert.assertEquals(PORT_DATA_4, unprocessedPortData.get(PORT_DATA_4.getPortUuid()));
+    }
+
+    private void processedAndUnprocessedDataAmountVerify(PortUuidToPortDataMapper portUuidToPortDataMapper,
+                                                         int processedDataCount,
+                                                         int unprocessedDataCount) {
+        HashMap<String, PortData> unprocessedDataMap = extractUnprocessedDataMap(portUuidToPortDataMapper);
+        HashMap<String, PortData> processedDataMap = extractProcessedDataMap(portUuidToPortDataMapper);
+
+        Assert.assertEquals(unprocessedDataCount, unprocessedDataMap.size());
+        Assert.assertEquals(processedDataCount, processedDataMap.size());
+    }
+
+    private HashMap<String, PortData> extractProcessedDataMap(PortUuidToPortDataMapper portUuidToPortDataMapper) {
+        HashMap<String, PortData> processedDataMap;
+        try {
+            processedDataMap = (HashMap<String, PortData>) Whitebox
+                    .getInternalState(portUuidToPortDataMapper, "processedPortUuidToEidMapper");
+        } catch (ClassCastException e) {
+            throw e;
+        }
+        return processedDataMap;
+    }
+
+    private HashMap<String, PortData> extractUnprocessedDataMap(PortUuidToPortDataMapper portUuidToPortDataMapper) {
+        HashMap<String, PortData> unprocessedDataMap;
+        try {
+            unprocessedDataMap = (HashMap<String, PortData>) Whitebox
+                    .getInternalState(portUuidToPortDataMapper, "unprocessedPortUuidToEidMapper");
+        } catch (ClassCastException e) {
+            throw e;
+        }
+        return unprocessedDataMap;
+    }
+}
