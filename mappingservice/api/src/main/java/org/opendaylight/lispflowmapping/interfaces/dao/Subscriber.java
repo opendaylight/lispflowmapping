8a2a3eb90c0d517743654a196198844f4892e133
Lorand Jakab <lojakab@cisco.com>
2017-02-21 00:10:29 +0200

    Add timeout to subscriber data
    The timeout of a subscriber is immutable too, and needs to be
    transmitted in a MappingChanged notification using the YANG modeled
    subscriber data.
    
    Change-Id: Id62485c79b92ba35c6cd682c78a888ed14234059
    Signed-off-by: Lorand Jakab <lojakab@cisco.com>

--- mappingservice/api/src/main/java/org/opendaylight/lispflowmapping/interfaces/dao/Subscriber.java
+++ mappingservice/api/src/main/java/org/opendaylight/lispflowmapping/interfaces/dao/Subscriber.java
@@ -11,8 +11,8 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.eid.container.Eid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.rloc.container.Rloc;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.subscriber.address.grouping.SubscriberAddress;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.subscriber.address.grouping.SubscriberAddressBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.subscriber.data.grouping.SubscriberData;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.lisp.proto.rev151105.subscriber.data.grouping.SubscriberDataBuilder;
 
 /**
  * Request source RLOC in the mapping service with it's properties.
@@ -23,9 +23,8 @@ public class Subscriber {
     // Subscriber timeout should be slightly higher than the mapping TTL. Arbitrary value is set to 10 minutes
     private static final int SUBSCRIBER_TIMEOUT_CONSTANT = 10;
 
-    private SubscriberAddress address;
+    private SubscriberData data;
     private Date lastRequestDate;
-    private int subscriberTimeout = DEFAULT_SUBSCRIBER_TIMEOUT;
 
     /**
      * Constructor.
@@ -48,21 +47,20 @@ public class Subscriber {
      */
     public Subscriber(Rloc srcRloc, Eid srcEid, int subscriberTimeout, Date lastRequestDate) {
         super();
-        this.address = new SubscriberAddressBuilder().setRloc(srcRloc).setEid(srcEid).build();
+        this.data = new SubscriberDataBuilder().setRloc(srcRloc).setEid(srcEid).setTimeout(subscriberTimeout).build();
         this.lastRequestDate = lastRequestDate;
-        this.subscriberTimeout = subscriberTimeout;
     }
 
-    public SubscriberAddress getSubscriberAddress() {
-        return address;
+    public SubscriberData getSubscriberData() {
+        return data;
     }
 
     public Rloc getSrcRloc() {
-        return address.getRloc();
+        return data.getRloc();
     }
 
     public Eid getSrcEid() {
-        return address.getEid();
+        return data.getEid();
     }
 
     public Date getLastRequestDate() {
@@ -74,21 +72,19 @@ public class Subscriber {
     }
 
     public int getSubscriberTimeout() {
-        return subscriberTimeout;
-    }
-
-    public void setSubscriberTimeout(int subscriberTimeout) {
-        this.subscriberTimeout = subscriberTimeout;
+        return data.getTimeout();
     }
 
     // Only used in MapResolverTest
     public void setSubscriberTimeoutByRecordTtl(Integer recordTtl) {
-        this.subscriberTimeout = recordTtlToSubscriberTime(recordTtl);
+        SubscriberDataBuilder sdb = new SubscriberDataBuilder(this.data);
+        sdb.setTimeout(recordTtlToSubscriberTime(recordTtl));
+        this.data = sdb.build();
     }
 
     /**
      * Static method to calculate the subscriber timeout from a mapping record TTL. If a mapping record TTL is not
-     * provided, use the default 1 day TTL. The suscriber timeout is the TTL plus a constant value.
+     * provided, use the default 1 day TTL. The subscriber timeout is the TTL plus a constant value.
      *
      * @param recordTtl The time to live (TTL) value
      * @return the subscriber timeout
@@ -102,12 +98,12 @@ public class Subscriber {
 
     public boolean timedOut() {
         return TimeUnit.MILLISECONDS
-                .toMinutes(System.currentTimeMillis() - lastRequestDate.getTime()) > subscriberTimeout;
+                .toMinutes(System.currentTimeMillis() - lastRequestDate.getTime()) > data.getTimeout();
     }
 
     @Override
     public int hashCode() {
-        return address.hashCode();
+        return data.hashCode();
     }
 
     @Override
@@ -122,7 +118,7 @@ public class Subscriber {
             return false;
         }
         Subscriber other = (Subscriber) obj;
-        if (!address.equals(other.address)) {
+        if (!data.equals(other.data)) {
             return false;
         }
         return true;
@@ -130,7 +126,7 @@ public class Subscriber {
 
     @Override
     public String toString() {
-        return "_rloc=" + address.getRloc().toString() + ", _eid=" + address.getEid().toString()
-                + ", last request @ " + lastRequestDate.toString();
+        return "_rloc=" + data.getRloc().toString() + ", _eid=" + data.getEid().toString()
+                + ", _timeout=" + data.getTimeout().toString() + ", last request @ " + lastRequestDate.toString();
     }
 }
