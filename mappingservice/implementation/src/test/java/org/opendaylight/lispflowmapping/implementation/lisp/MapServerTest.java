e3c3f06641bd96805c38e512c5a2d578f7a7da2c
Lorand Jakab <lojakab@cisco.com>
2017-04-04 19:20:14 +0300

    Bug 7947: Store MappingOrigin in MappingData
    As a first step towards separate subscriber storage, based on origin,
    this patch adds support for storing the origin of a mapping in the
    MappingData object. The origin is defined as a Set, to support the use
    case of returning a combined mapping with information from different
    origins, such as the NB_AND_SB policy surrently.
    
    This patch doesn't change behavior yet, it just stores the origin.
    
    Change-Id: I930fd101dc2b7ca495a9454997d968111901a1b3
    Signed-off-by: Lorand Jakab <lojakab@cisco.com>

--- mappingservice/implementation/src/test/java/org/opendaylight/lispflowmapping/implementation/lisp/MapServerTest.java
+++ mappingservice/implementation/src/test/java/org/opendaylight/lispflowmapping/implementation/lisp/MapServerTest.java
@@ -32,9 +32,9 @@ import org.opendaylight.lispflowmapping.interfaces.dao.Subscriber;
 import org.opendaylight.lispflowmapping.interfaces.lisp.IMapNotifyHandler;
 import org.opendaylight.lispflowmapping.interfaces.mappingservice.IMappingService;
 import org.opendaylight.lispflowmapping.lisp.type.LispMessage;
-import org.opendaylight.lispflowmapping.lisp.type.MappingData;
 import org.opendaylight.lispflowmapping.lisp.util.LispAddressUtil;
 import org.opendaylight.lispflowmapping.lisp.util.SourceDestKeyHelper;
+import org.opendaylight.lispflowmapping.type.MappingData;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev130715.PortNumber;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.inet.binary.types.rev160303.IpAddressBinary;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.lfm.inet.binary.types.rev160303.Ipv4AddressBinary;
@@ -143,8 +143,10 @@ public class MapServerTest {
             .setLocatorRecord(Lists.newArrayList(LOCATOR_RECORD_1)).build();
     private static final MappingRecord OLD_MAPPING_RECORD_2 = getDefaultMappingRecordBuilder()
             .setLocatorRecord(Lists.newArrayList(LOCATOR_RECORD_2)).build();
-    private static final MappingData OLD_MAPPING_DATA_1 = new MappingData(OLD_MAPPING_RECORD_1);
-    private static final MappingData OLD_MAPPING_DATA_2 = new MappingData(OLD_MAPPING_RECORD_2);
+    private static final MappingData OLD_MAPPING_DATA_1 = new MappingData(MappingOrigin.Southbound,
+            OLD_MAPPING_RECORD_1);
+    private static final MappingData OLD_MAPPING_DATA_2 = new MappingData(MappingOrigin.Southbound,
+            OLD_MAPPING_RECORD_2);
 
     private static final Set<IpAddressBinary> DEFAULT_IP_ADDRESS_SET = getDefaultIpAddressSet();
 
@@ -158,7 +160,8 @@ public class MapServerTest {
         subscriberSetMock_3.add(SUBSCRIBER_RLOC_5);
         subscriberSetMock_3.add(SUBSCRIBER_RLOC_6);
         mapRegister = getDefaultMapRegisterBuilder().build();
-        mappingData = new MappingData(mapRegister.getMappingRecordItem().iterator().next().getMappingRecord(),
+        mappingData = new MappingData(MappingOrigin.Southbound,
+                mapRegister.getMappingRecordItem().iterator().next().getMappingRecord(),
                 System.currentTimeMillis());
         setConfigIniMappingMergeField(false);
     }
@@ -508,7 +511,7 @@ public class MapServerTest {
         if (mappingRecord == null) {
             mappingRecord = getDefaultMappingRecordBuilder().build();
         }
-        return new MappingData(mappingRecord, System.currentTimeMillis());
+        return new MappingData(MappingOrigin.Southbound, mappingRecord, System.currentTimeMillis());
     }
 
     private static MappingRecordItemBuilder getDefaultMappingRecordItemBuilder() {
