e3cd2f115299d8fa833983e828169acd47e84d55
Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
2023-01-26 17:43:09 +0200

    Remove blueprint XML from shell component
    Karaf framework for custom shell commands updated.
    
    Change-Id: I932cc837b975ed9a1109391bbc0563a24fe074cb
    Signed-off-by: Ruslan Kashapov <ruslan.kashapov@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- mappingservice/shell/src/main/java/org/opendaylight/lispflowmapping/shell/LispAddKey.java
+++ mappingservice/shell/src/main/java/org/opendaylight/lispflowmapping/shell/LispAddKey.java
@@ -5,33 +5,31 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.lispflowmapping.shell;
 
-import org.apache.karaf.shell.commands.Command;
-import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.opendaylight.lispflowmapping.interfaces.mappingservice.IMappingServiceShell;
 
 /**
  * This class implements the "mappingservice:addkey" Karaf shell command.
  *
  * @author Lorand Jakab
- *
  */
+@Service
 @Command(scope = "mappingservice", name = "addkey", description = "Add an authentication key")
-public class LispAddKey  extends OsgiCommandSupport {
+public class LispAddKey implements Action {
+    @Reference
     private IMappingServiceShell mappingServiceShell;
 
     @Override
-    protected Object doExecute() throws Exception {
+    public Object execute() throws Exception {
         if (mappingServiceShell != null) {
             mappingServiceShell.addDefaultKeyIPv4();
             mappingServiceShell.addDefaultKeyIPv6();
         }
         return null;
-    }
-
-    public void setMappingServiceShell(IMappingServiceShell mappingServiceShell) {
-        this.mappingServiceShell = mappingServiceShell;
     }
 }
--- mappingservice/shell/src/main/java/org/opendaylight/lispflowmapping/shell/LispMappings.java
+++ mappingservice/shell/src/main/java/org/opendaylight/lispflowmapping/shell/LispMappings.java
@@ -5,30 +5,31 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.lispflowmapping.shell;
 
-import org.apache.karaf.shell.commands.Command;
-import org.apache.karaf.shell.commands.Option;
-import org.apache.karaf.shell.console.OsgiCommandSupport;
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Option;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.opendaylight.lispflowmapping.interfaces.mappingservice.IMappingServiceShell;
 
 /**
  * This class implements the "mappingservice:mappings" Karaf shell command.
  *
  * @author Lorand Jakab
- *
  */
+@Service
 @Command(scope = "mappingservice", name = "mappings", description = "Print mapping database")
-public class LispMappings  extends OsgiCommandSupport {
+public class LispMappings implements Action {
     @Option(name = "-d", aliases = "--debug", description = "Debug output", required = false, multiValued = false)
     private boolean debug;
-
+    @Reference
     private IMappingServiceShell mappingServiceShell;
 
     @Override
     @SuppressWarnings("checkstyle:RegexpSinglelineJava")
-    protected Object doExecute() throws Exception {
+    public Object execute() throws Exception {
         if (mappingServiceShell != null) {
             if (debug) {
                 System.out.print(mappingServiceShell.printMappings());
@@ -37,9 +38,5 @@ public class LispMappings  extends OsgiCommandSupport {
             }
         }
         return null;
-    }
-
-    public void setMappingServiceShell(IMappingServiceShell mappingServiceShell) {
-        this.mappingServiceShell = mappingServiceShell;
     }
 }
